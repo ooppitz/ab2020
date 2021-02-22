ac8738c9792114e625f1f9c02f5b98c3f9b74b37
Felix <Felix@DESKTOP-KRQH503.fritz.box>
2021-02-22 11:30:52 +0100

    For the Empire!

--- AB2020/src/de/die_gfi/felix/FelixTest.java
+++ AB2020/src/de/die_gfi/felix/FelixTest.java
@@ -3,6 +3,7 @@ package de.die_gfi.felix;
 public class FelixTest {
 	public static void main(String args[]) {
 		System.out.println("Nobody expects the Spanish Inquisition!");
+		System.out.println("For the Empire!");
 	}
 
 }
