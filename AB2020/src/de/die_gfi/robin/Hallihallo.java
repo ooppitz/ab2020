1946955f87fb3f749f4aa0eaf740185f276ebacb
Nutzer <Nutzer@DESKTOP-C9INLEB.local>
2021-02-22 13:16:46 +0100

    meine neune Aenderung

--- AB2020/src/de/die_gfi/robin/Hallihallo.java
+++ AB2020/src/de/die_gfi/robin/Hallihallo.java
@@ -5,5 +5,6 @@ public class Hallihallo {
 	public static void main(String[] args) {
 		System.out.println("Hallo Leute!");
 		System.out.println("Willkommen in meinem Repositorium!!");
+		System.out.println("Das ist die neueste Änderung lalala");
 	}
 }
