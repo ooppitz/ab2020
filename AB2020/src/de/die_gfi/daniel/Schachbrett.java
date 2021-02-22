fc14105a8afa5cee75ec06828bc6dee16475d45b
Daniel <Daniel@PC04>
2021-02-22 14:53:32 +0000

    Erste funktionierende Version

--- AB2020/src/de/die_gfi/daniel/Schachbrett.java
+++ AB2020/src/de/die_gfi/daniel/Schachbrett.java
@@ -5,24 +5,27 @@ public class Schachbrett
    public static void main( String[] args )
    {
       double koerner = 1;     /* Anzahl der Koerner */
-      int feld = 1;
                         
       int zeilen;             /* Laufvariable fuer die Zeilen vom Schachbrett     */
       int spalten;            /* Laufvariable fuer die Spalten vom Schachbrett    */
       
       
-      
+      System.out.printf( "+------------+------------+------------+------------+------------+------------+------------+------------+\n" );
       
       for( zeilen = 0; zeilen < 8; zeilen++ )
       {
+         System.out.printf( "|" );
+         
          for( spalten = 0; spalten < 8; spalten++ )
          {
-            System.out.printf( "feld %2d: %.2E\n", feld, koerner );
-            feld++;
+            System.out.printf( " %10.4E |", koerner );
+            
             koerner = koerner * 2;
          }
          
          System.out.printf( "\n" );
+         System.out.printf( "+------------+------------+------------+------------+------------+------------+------------+------------+\n" );
+
       }
       
       
