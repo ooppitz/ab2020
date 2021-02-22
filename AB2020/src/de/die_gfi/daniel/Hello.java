08145867cbd287115ebb740018be717acd245541
Daniel <Daniel@PC04>
2021-02-22 10:38:28 +0000

    Weitere Ausgabe von "Hello World" hinzugefuegt

--- AB2020/src/de/die_gfi/daniel/Hello.java
+++ AB2020/src/de/die_gfi/daniel/Hello.java
@@ -6,6 +6,7 @@ public class Hello
    public static void main(String[] args)
    {
       System.out.printf( "Hello World\n" );
+      System.out.printf( "Hello World\n" );
 
       System.out.printf( "Programmende\n" );
    }
