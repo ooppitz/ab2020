a88522b956272cf437a9aab33fd30ac5dad097d1
Daniel <Daniel@PC04>
2021-02-22 15:24:13 +0000

    Rahmen mit Beschriftung hinzugefuegt

--- AB2020/src/de/die_gfi/daniel/Schachbrett.java
+++ AB2020/src/de/die_gfi/daniel/Schachbrett.java
@@ -10,21 +10,24 @@ public class Schachbrett
       int spalten;            /* Laufvariable fuer die Spalten vom Schachbrett    */
       
       
-      System.out.printf( "+------------+------------+------------+------------+------------+------------+------------+------------+\n" );
+      System.out.printf( "+---+------------+------------+------------+------------+------------+------------+------------+------------+\n" );
+      System.out.printf( "|   |     AA     |     BB     |     CC     |     DD     |     EE     |     FF     |     GG     |     HH     |\n" );
+      System.out.printf( "+---+------------+------------+------------+------------+------------+------------+------------+------------+\n" );
+
       
       for( zeilen = 0; zeilen < 8; zeilen++ )
       {
-         System.out.printf( "|" );
+         System.out.printf( "| %d |", zeilen + 1 );
          
          for( spalten = 0; spalten < 8; spalten++ )
          {
-            System.out.printf( " %10.4E |", koerner );
+            System.out.printf( " %.4E |", koerner );
             
             koerner = koerner * 2;
          }
          
          System.out.printf( "\n" );
-         System.out.printf( "+------------+------------+------------+------------+------------+------------+------------+------------+\n" );
+         System.out.printf( "+---+------------+------------+------------+------------+------------+------------+------------+------------+\n" );
 
       }
       
