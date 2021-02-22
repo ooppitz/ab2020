1b916a86f759b8c3b11fd6fd8155135ac7abc0ab
mhhb0570 <mhhb0570@md2krmyc.ad001.siemens.net>
2021-02-22 14:43:05 +0100

    klasse schleife rückwärts erstellt

---
+++ AB2020/src/de/die_gfi/eva/SchleifeRückwärts.java
@@ -0,0 +1,13 @@
+package de.die_gfi.eva;
+
+public class SchleifeRückwärts {
+
+	public static void main(String[] args) {
+		
+		for (int i = 100; i > 0; i--) {
+			System.out.println("i = " + i);
+			
+		}
+	}
+
+}
