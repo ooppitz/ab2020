dc6cfd8bbe26df807c8fa4fbebfbe9d3f44628f9
jonathan <jonathan@lenovo>
2021-02-22 15:27:56 +0100

    berechnung der Zahlen

---
+++ AB2020/src/de/die_gfi/jonathan/Schachbrett.java
@@ -0,0 +1,12 @@
+package de.die_gfi.jonathan;
+//schachbrett rätsel simulieren aufs erte ein reiskorn 
+//auf nächste doppelt so viele bis zum ende
+public class Schachbrett {
+public static void main(String[] args) {
+	int reis=1
+	for (int i = 0; i < 64; i++) {
+		reis=reis*2;
+		System.out.println(reis);
+	}
+}
+}
