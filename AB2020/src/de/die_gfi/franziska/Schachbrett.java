b25f611d7516abe738eb94e159492562dc2267be
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-02-23 10:51:22 +0100

    stopped attempt

--- AB2020/src/de/die_gfi/franziska/Schachbrett.java
+++ AB2020/src/de/die_gfi/franziska/Schachbrett.java
@@ -3,8 +3,33 @@ package de.die_gfi.franziska;
 public class Schachbrett {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double zahl = 0;
+		
+		boolean bigNumber = zahl>10000000000l;
+		
+		for (int i = 0; i < 64; i++) {
+			
+			zahl = Math.pow(2, i);
+			
+			if (bigNumber) {
+			
+			zahl = zahl/10000000000l;
+			
+		}
+			System.out.println(zahl);
+			
+		}
+		
+		
+		
+		for (int i = 0; i < 9; i++) {
+			
+			System.out.println("|   " + zahl +"   |   " + zahl + "   |   " + zahl +"   |   " + zahl + "   |   " + zahl + "   |   " + zahl + "   |   " + zahl + "   |   " + zahl + "   |   ");
+			System.out.println("+.......+.......+.......+.......+.......+.......+.......+.......+");
+			
+		}	
+		
 	}
 
 }
