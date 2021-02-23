c5690be1d999da811ab08f26ccd685681f5b8e38
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-02-23 11:57:06 +0100

    Merge conflict resolve

--- AB2020/src/de/die_gfi/jonathan/Schachbrett.java
+++ AB2020/src/de/die_gfi/jonathan/Schachbrett.java
@@ -1,26 +1,12 @@
 package de.die_gfi.jonathan;
-//schachbrett rätsel simulieren aufs erte ein  
-//reiskorn auf nächste doppelt so viele bis zum ende
-public class Schachbrett {public static void main(String[] args) {
+//schachbrett rätsel simulieren aufs erte ein reiskorn 
+//auf nächste doppelt so viele bis zum ende
+public class Schachbrett {
+public static void main(String[] args) {
 	double reis=1;
-	double[] reiskorn = new double[64] ;
-	String[] rice = new String [64];
 	for (int i = 0; i < 64; i++) {
-		reiskorn[i]=reis;
-
 		reis=reis*2;
-		
-}
-	int mazziff=zifferAnzahl(reiskorn[64]);
-	for (int j = 0; j < reiskorn.length; j++) {
-		rice[j]=Double.toString(reiskorn[j]); 
-		
+		System.out.println(reis);
 	}
 }
-public static int zifferAnzahl(double z) {
-	String t=Double.toString(z); 
-	int a= t.toCharArray().length;
-	return a;
-}
-
 }
