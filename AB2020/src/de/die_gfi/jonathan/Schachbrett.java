23d86affce8be80a3212e638e8e731783dc493c2
jonathan <jonathan@lenovo>
2021-02-23 10:47:52 +0100

    externbearbeitet

--- AB2020/src/de/die_gfi/jonathan/Schachbrett.java
+++ AB2020/src/de/die_gfi/jonathan/Schachbrett.java
@@ -1,17 +1,29 @@
 package de.die_gfi.jonathan;
 //schachbrett rätsel simulieren aufs erte ein  
 //reiskorn auf nächste doppelt so viele bis zum ende
-public class Schachbrett {
-public static void main(String[] args) {
+public class Schachbrett {public static void main(String[] args) {
 	double reis=1;
 	double[] reiskorn = new double[64] ;
+	String[] rice = new String [64];
 	for (int i = 0; i < 64; i++) {
 		reiskorn[i]=reis;
+
 		reis=reis*2;
-		System.out.print(reis+"		");
-		if (i%7==0) {
-		System.out.print("\n");	
-		}
+		//System.out.print(reis+"		");
+		//if (i%7==0) {
+		//System.out.print("\n");	
+		//}
+}
+	int mazziff=zifferAnzahl(reiskorn[64]);
+	for (int j = 0; j < reiskorn.length; j++) {
+		rice[j]=Double.toString(reiskorn[j]); 
+		
 	}
 }
+public static int zifferAnzahl(double z) {
+	String t=Double.toString(z); 
+	int a= t.toCharArray().length;
+	return a;
+}
+
 }
