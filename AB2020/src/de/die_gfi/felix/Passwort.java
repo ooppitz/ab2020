495e327d064f2a0e8c6f04467d287f24ceb735a6
Oliver Oppitz <ooppitz@gmail.com>
2021-02-24 15:31:46 +0100

    Improve felix.Passwort.

--- AB2020/src/de/die_gfi/felix/Passwort.java
+++ AB2020/src/de/die_gfi/felix/Passwort.java
@@ -6,60 +6,47 @@ import java.util.ArrayList;
 
 public class Passwort {
 	public static void main(String args[]) {
-		
-		ArrayList<String> passwortListe = new ArrayList<String>();
-		boolean schalter2 = false;
-		String hashcode = "";
+
+		ArrayList<String> hashListe = new ArrayList<String>();
+
 		Scanner sc = new Scanner(System.in);
-		
-		while(schalter2 == false) {
-			
-			String passwort = sc.next();
-			
-			hashcode = hashcodeErmittlung(passwort);
-			
-			if(passwortListe.size() > 0) {
-				int index = passwortListe.indexOf(hashcode);
-				if(index == -1) {
-					System.out.println("Weiteres Passwort hinzugefügt");
-					passwortListe.add(hashcode);
-				} else {
-					System.out.println("Passwort bereits Vorhanden");
-				}
-				
-			} else {
-				passwortListe.add(hashcode);
-			}
-			
-			System.out.println("Möchten Sie noch ein Passwort setzen? Wenn Ja geben Sie 'xxxJa' ein");
-			String weiteresPasswortGewünscht = sc.next();
-			if(weiteresPasswortGewünscht.equals("xxxJa")) {
-				schalter2 = false;
-			} else {
-				schalter2 = true;
-			}
-		}	
-		
-		//------------------------
+
+		passwortFestlegen(hashListe, sc);
+
+		passwortUeberpruefen(hashListe, sc);
+
+		sc.close();
+
+	}
+
+	/**
+	 * Der Benutzer kann ein Passwort eingeben. Es wird Ã¼berprÃ¼ft anhand des Hashwertes,
+	 * ob dieses in der Liste der PasswÃ¶rter vorhanden ist. Der Benutzer hat drei Versuche.
+	 * Sind diese aufgebraucht, beendet sich das Programm.
+	 * 
+	 * @param passwortListe
+	 * @param sc
+	 */
+	private static void passwortUeberpruefen(ArrayList<String> passwortListe, Scanner sc) {
 		
 		System.out.println("Bitte geben Sie das Passwort ein um Fortzufahren");
 		int zaehler = 0;
 		boolean schalter = false;
-		
-		while(schalter == false ) {
+
+		while (schalter == false) {
 			String passwort2 = sc.next();
-		
+
 			String hashcode2 = hashcodeErmittlung(passwort2);
 			int index = passwortListe.indexOf(hashcode2);
-			
-			if(index != -1) {
+
+			if (index != -1) {
 				System.out.println("Korrekte Eingabe");
 				schalter = true;
 				break;
 			} else {
 				System.out.println("Inkoorekte Eingabe");
 				zaehler++;
-				if(zaehler >= 3) {
+				if (zaehler >= 3) {
 					System.out.println("Drei Inkorrekte Eingabe versuche. Programm wird beendet");
 					sc.close();
 					System.exit(0);
@@ -68,18 +55,51 @@ public class Passwort {
 				}
 			}
 		}
-	sc.close();
-	
 	}
-	
+
+	/**
+	 * LÃ¤sst den User ein erstes Passwort eingeben und speichert dessen Hashcode.
+	 * AnschlieÃŸend bekommt der User die MÃ¶glichkeit, weitere PasswÃ¶rter einzugeben.
+	 * Gegebenenfalls werden die Hashcodes gespeichert.
+	 * 
+	 * @param hashListe
+	 * @param sc
+	 */
+	private static void passwortFestlegen(ArrayList<String> hashListe, Scanner sc) {
+
+		String hashcode;
+		String weiteresPasswortGewuenscht;
+		
+		do {
+			System.out.println("Geben Sie ein Passwort ein: ");
+			String passwort = sc.nextLine();
+
+			hashcode = hashcodeErmittlung(passwort);
+
+			int index = hashListe.indexOf(hashcode);
+			if (index == -1) {
+				System.out.println("Weiteres Passwort hinzugefï¿½gt");
+				hashListe.add(hashcode);
+			} else {
+				System.out.println("Passwort bereits Vorhanden");
+			}
+
+			System.out.println("Mï¿½chten Sie noch ein Passwort setzen? Wenn Ja geben Sie 'xxxJa' ein");
+			weiteresPasswortGewuenscht = sc.nextLine();
+
+		} while(weiteresPasswortGewuenscht.equals("xxxJa"));
+		
+		System.out.println("Ende von PasswortFestlegen()");
+		
+	}
+
 	public static String hashcodeErmittlung(String passwort) {
+
 		char[] passwortArr = passwort.toCharArray();
 		int hashcode = Arrays.hashCode(passwortArr);
 		String hashcodeString = String.valueOf(hashcode);
 		return hashcodeString;
-		
+
 	}
-	
-	
-	
+
 }
