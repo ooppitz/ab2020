48fff688b45db12d39144b90cfe4e35d4f7c7ee7
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-02-24 11:49:39 +0100

    created class HashExample

---
+++ AB2020/src/de/die_gfi/franziska/HashExample.java
@@ -0,0 +1,30 @@
+package de.die_gfi.franziska;
+
+import java.util.Arrays;
+
+public class HashExample {
+
+	public static void main(String[] args) {
+
+		int[] arr1 = new int[4];
+
+		arr1[0] = 4;
+
+		for (int i = 0; i < arr1.length; i++) {
+
+			System.out.println("Inhalte: " + arr1[i]);
+		}
+
+		System.out.println("\nHashcode: " + Arrays.hashCode(arr1) + "\n");
+
+		arr1[0] = 28;
+
+		for (int i = 0; i < arr1.length; i++) {
+
+			System.out.println("Inhalte nach Änderung: " + arr1[i]);
+		}
+
+		System.out.println("\nHashcode: " + Arrays.hashCode(arr1));
+
+	}
+}
