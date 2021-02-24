8461342e80ce0f8e3182d54521bdac29bce427a4
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-02-24 13:35:05 +0100

    Also completed part 2

--- AB2020/src/de/die_gfi/philipp/PasswordsComparisonThing.java
+++ AB2020/src/de/die_gfi/philipp/PasswordsComparisonThing.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 
 public class PasswordsComparisonThing {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
         System.out.print("Please enter a password: ");
-        int firstHash = Arrays.hashCode(input.nextLine().toCharArray());
+        int firstHash = readPassword();
         System.out.print("\n".repeat(800));
 
         System.out.print("Please enter another password: ");
-        int secondHash = Arrays.hashCode(input.nextLine().toCharArray());
+        int secondHash = readPassword();
 
         if (firstHash == secondHash) {
             System.out.println("The two entered passwords are the same.");
         } else {
             System.out.println("The two entered passwords are not the same.");
         }
+    }
+
+    public static int readPassword() {
+        return Arrays.hashCode((new Scanner(System.in)).nextLine().toCharArray());
     }
 }
