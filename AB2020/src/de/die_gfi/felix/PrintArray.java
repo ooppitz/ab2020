c50555eb06ff55d5b3aee4e36ef194d7df036838
Felix <Felix@DESKTOP-KRQH503.fritz.box>
2021-02-24 09:54:58 +0100

    Umlaute ersetzen

--- AB2020/src/de/die_gfi/felix/PrintArray.java
+++ AB2020/src/de/die_gfi/felix/PrintArray.java
@@ -5,10 +5,10 @@ import de.die_gfi.util.Print;
 public class PrintArray {
 	public static void main(String args[]) {
 		int[] intArr = new int[242 - 2 + 1]; 
-		int zähler = 2;
+		int zaehler = 2;
 		for(int i = 0; i < intArr.length; i++) {
-			intArr[i] = zähler;
-			zähler++;
+			intArr[i] = zaehler;
+			zaehler++;
 		}
 	Print.printArray(intArr);	
 	}
