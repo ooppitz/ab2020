157ec8cf7fb82ca3ffaef61e7e0f3fa3956d63a2
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-02-24 09:44:51 +0100

    fixed another small mistake

--- AB2020/src/de/die_gfi/franziska/PrintArray.java
+++ AB2020/src/de/die_gfi/franziska/PrintArray.java
@@ -1,21 +1,18 @@
 package de.die_gfi.franziska;
 
-
 public class PrintArray {
 
 	public static void main(String[] args) {
 
-		int[] arr = new int[243];
-		
+		int[] arr = new int[241];
+
 		for (int i = 0; i < arr.length; i++) {
-			
-			arr[i] = i +2;
+
+			arr[i] = i + 2;
 		}
-		
+
 		de.die_gfi.util.Print.printArray(arr);
-		
-		
-		
+
 	}
 
 }
