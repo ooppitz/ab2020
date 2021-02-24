260b522cdaef674536e0ad50322612f630a22d21
Felix <Felix@DESKTOP-KRQH503.fritz.box>
2021-02-24 14:31:00 +0100

    Aufgabe 3 Implementierung Erster Teil

--- AB2020/src/de/die_gfi/felix/Passwort.java
+++ AB2020/src/de/die_gfi/felix/Passwort.java
@@ -2,24 +2,57 @@ package de.die_gfi.felix;
 
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Passwort {
 	public static void main(String args[]) {
+		
+		ArrayList<String> passwortListe = new ArrayList<String>();
+		boolean schalter2 = false;
+		String hashcode = "";
 		Scanner sc = new Scanner(System.in);
-		String passwort = sc.next();
-		int hashcode = hashcodeErmittlung(passwort);
+		
+		while(schalter2 == false) {
+			
+			String passwort = sc.next();
+			
+			hashcode = hashcodeErmittlung(passwort);
+			
+			if(passwortListe.size() > 0) {
+				int index = passwortListe.indexOf(hashcode);
+				if(index == -1) {
+					System.out.println("Weiteres Passwort hinzugefügt");
+					passwortListe.add(hashcode);
+				} else {
+					System.out.println("Passwort bereits Vorhanden");
+				}
+				
+			} else {
+				passwortListe.add(hashcode);
+			}
+		System.out.println("Möchten Sie noch ein Passwort setzen? Wenn Ja geben Sie 'xxxJa' ein");
+		String weiteresPasswort = sc.next();
+		if(weiteresPasswort.equals("xxxJa")) {
+			schalter2 = false;
+		} else {
+			schalter2 = true;
+		}
+		}	
+		
+		
+		
+		
+		//------------------------
 		
 		System.out.println("Bitte geben Sie das Passwort ein um Fortzufahren");
-		
 		int zaehler = 0;
 		boolean schalter = false;
-		
 		
 		while(schalter == false ) {
 			String passwort2 = sc.next();
 		
-			int hashcode2 = hashcodeErmittlung(passwort2);
-			if(hashcode == hashcode2) {
+			String hashcode2 = hashcodeErmittlung(passwort2);
+			if(hashcode.equals(hashcode2)) {
 				System.out.println("Korrekte Eingabe");
 				schalter = true;
 				break;
@@ -28,21 +61,25 @@ public class Passwort {
 				zaehler++;
 				if(zaehler >= 3) {
 					System.out.println("Drei Inkorrekte Eingabe versuche. Programm wird beendet");
+					sc.close();
 					System.exit(0);
 				} else {
 					System.out.println("Versuchen Sie es erneut");
 				}
+			}
 		}
-		}
-	
+	sc.close();
 	
 	}
 	
-	public static int hashcodeErmittlung(String passwort) {
+	public static String hashcodeErmittlung(String passwort) {
 		char[] passwortArr = passwort.toCharArray();
 		int hashcode = Arrays.hashCode(passwortArr);
-		return hashcode;
+		String hashcodeString = String.valueOf(hashcode);
+		return hashcodeString;
 		
 	}
+	
+	
 	
 }
