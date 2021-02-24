5af43d8d3e9aee5e89783fdce2c28aad376ac1ed
bfz <bfz@HomeO-NB-083101>
2021-02-24 08:32:06 +0100

    playgroud erstes commitment

---
+++ AB2020/src/de/die_gfi/jonathan/Spielwiese.java
@@ -0,0 +1,13 @@
+package de.die_gfi.jonathan;
+
+import java.util.Arrays;
+
+public class Spielwiese {
+
+	public static void main(String[] args) {
+		int[] a = { 1, 10, 100, 1000, 10000 };
+        int[] b = { 1, 10, 100, 1000, 10000 };
+        Arrays.fill(b, 1, 3, 99);
+	}
+
+}
