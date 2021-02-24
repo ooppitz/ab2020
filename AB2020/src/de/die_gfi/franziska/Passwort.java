d8c6f4f6f800dda113ae70d469cc28869c704370
Oliver Oppitz <ooppitz@gmail.com>
2021-02-24 13:43:58 +0100

    Reorder code

--- AB2020/src/de/die_gfi/franziska/Passwort.java
+++ AB2020/src/de/die_gfi/franziska/Passwort.java
@@ -7,32 +7,41 @@ public class Passwort {
 
 	public static void main(String[] args) {
 
+	
+		int hash1; 
+		
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Bitte Passwort eingeben");
-
+	
 		String passwort1 = scanner.nextLine();
 
+		char[] zeichen1 = passwort1.toCharArray();
+		
+		hash1 = Arrays.hashCode(zeichen1);
+		
+		
+		
 		System.out.println("Bitte Passwort erneut eingeben");
 
 		String passwort2 = scanner.nextLine();
 
 		scanner.close();
 
-		char[] zeichen1 = passwort1.toCharArray();
+		
 
 		char[] zeichen2 = passwort2.toCharArray();
 
-		int hash1 = Arrays.hashCode(zeichen1);
+	  
 
 		int hash2 = Arrays.hashCode(zeichen2);
 
 		if (hash1 == hash2) {
 
-			System.out.println("Die Passwörter sind gleich.");
+			System.out.println("Die Passwï¿½rter sind gleich.");
 		} else {
 
-			System.out.println("Die Passwörter unterscheiden sich.");
+			System.out.println("Die Passwï¿½rter unterscheiden sich.");
 		}
 
 	}
