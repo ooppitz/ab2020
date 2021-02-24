784a1d049cadf89eb010b2bf9e4e414b36852f12
Nutzer <Nutzer@DESKTOP-C9INLEB.local>
2021-02-24 08:46:56 +0100

    commit2

--- AB2020/src/de/die_gfi/robin/ArraySortieren.java
+++ AB2020/src/de/die_gfi/robin/ArraySortieren.java
@@ -6,14 +6,32 @@ public class ArraySortieren {
 	public static void main(String[] args) {
 		
 		int[] arr = new int[] {0,2,4,-6,8,-10};
-		
 		Arrays.sort(arr);
+		
 		
 		for (int eintrag : arr) {
 			System.out.print(eintrag + " ");
 		}
 		
-		int[] arrCopy = new arr
+		System.out.println("");
+		
+		
+		
+		int length = arr.length;
+		int[] arrCopy = new int[length + 1];
+		
+		
+		for (int i = 0; i < length; i++) {
+			arrCopy[i] = arr[i];
+		}
+		
+		
+		arrCopy[length] = 100;
+		
+		for (int eintrag : arrCopy) {
+			System.out.print(eintrag + " ");
+		}
+		
 		
 		
 	}
