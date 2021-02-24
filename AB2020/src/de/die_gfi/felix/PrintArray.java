0b3bd0ffb754b4835aabbc47db3c7e7a462d0209
Oliver Oppitz <ooppitz@gmail.com>
2021-02-24 09:57:11 +0100

    Change PrintArray

--- AB2020/src/de/die_gfi/felix/PrintArray.java
+++ AB2020/src/de/die_gfi/felix/PrintArray.java
@@ -1,12 +1,14 @@
 package de.die_gfi.felix;
 
 public class PrintArray {
+	
+	
 	public static void main(String args[]) {
 		int[] intArr = new int[242 - 2 + 1]; 
-		int zähler = 2;
+		int zï¿½hler = 2;
 		for(int i = 0; i < intArr.length; i++) {
-			intArr[i] = zähler;
-			zähler++;
+			intArr[i] = zï¿½hler;
+			zï¿½hler++;
 		}
 	de.die_gfi.util.Print.printArray(intArr);	
 	}
--- AB2020/src/de/die_gfi/jonathan/PrintArray.java
+++ AB2020/src/de/die_gfi/jonathan/PrintArray.java
@@ -2,16 +2,23 @@ package de.die_gfi.jonathan;
 
 import de.die_gfi.util.Print;
 
-
 public class PrintArray {
 
 	public static void main(String[] args) {
-		int [] b=new int [241];
+		
+		// 2 ... 242
+		// 2 .. 3   ...   3-2 = 1 
+		
+		int upperBound = 242;
+		int lowerBound = 2;
+		int count = upperBound - lowerBound + 1;
+		
+		int[] b = new int[count];
+		
 		for (int i = 0; i < b.length; i++) {
-			b[i]=i+2;
+			b[i] = i + 2;
 		}
 		Print.printArray(b);
 	}
-	
 
 }
