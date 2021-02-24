d8c4afd6d7d55c47c65119745b9c77e4554fe186
bfz <bfz@HomeO-NB-083101>
2021-02-24 09:37:41 +0100

    working

--- AB2020/src/de/die_gfi/jonathan/PrintArray.java
+++ AB2020/src/de/die_gfi/jonathan/PrintArray.java
@@ -1,16 +1,16 @@
 package de.die_gfi.jonathan;
 
-import java.text.
+import de.die_gfi.util.Print;
 
 
 public class PrintArray {
 
 	public static void main(String[] args) {
-		int [] b=new int [240];
+		int [] b=new int [241];
 		for (int i = 0; i < b.length; i++) {
 			b[i]=i+2;
 		}
-		Print.printArray(int[] b);
+		Print.printArray(b);
 	}
 	
 
