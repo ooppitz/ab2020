afba68133f66b5c6a39ce4e3f7ad93ddca64b2f6
mhhb0570 <mhhb0570@md2krmyc.ad001.siemens.net>
2021-02-24 09:46:54 +0100

    class PrintArray

---
+++ AB2020/src/de/die_gfi/eva/PrintArray.java
@@ -0,0 +1,20 @@
+package de.die_gfi.eva;
+
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[241];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 2;
+		}
+		
+		de.die_gfi.util.Print.printArray(arr);
+		
+		
+		
+	}
+
+}
