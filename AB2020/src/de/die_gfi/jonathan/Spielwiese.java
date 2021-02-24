661dd8fcfdd13dbfc7691ce40bec2e620b810708
bfz <bfz@HomeO-NB-083101>
2021-02-24 08:38:23 +0100

    spielwiese funktioning

--- AB2020/src/de/die_gfi/jonathan/Spielwiese.java
+++ AB2020/src/de/die_gfi/jonathan/Spielwiese.java
@@ -8,6 +8,9 @@ public class Spielwiese {
 		int[] a = { 1, 10, 100, 1000, 10000 };
         int[] b = { 1, 10, 100, 1000, 10000 };
         Arrays.fill(b, 1, 3, 99);
+        for (int i : b) {
+			System.out.println(i+" "+b);
+		}
 	}
 
 }
