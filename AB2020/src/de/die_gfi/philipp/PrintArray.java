c492c03a69526eb156e0f212972ec009b0d64a04
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-02-24 10:04:09 +0100

    Full change of logic, to make the program more visual in how it is structured and what it is doing.

--- AB2020/src/de/die_gfi/philipp/PrintArray.java
+++ AB2020/src/de/die_gfi/philipp/PrintArray.java
@@ -4,10 +4,13 @@ import de.die_gfi.util.Print;
 
 public class PrintArray {
     public static void main(String[] args) {
-        int[] intArray = new int[241];
+        int lowerBound = 2, upperBound = 242, index = 0;
+        int[] intArray = new int[upperBound - lowerBound + 1];
 
-        for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = i + 2;
+        int counter = lowerBound;
+        while (counter <= upperBound) {
+            intArray[index] = counter++;
+            index++;
         }
 
         Print.printArray(intArray);
