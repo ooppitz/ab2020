267d3fb4fc114c2e9af0492d2b0b9637d77f8566
Nutzer <Nutzer@DESKTOP-C9INLEB.local>
2021-02-24 13:55:01 +0100

    dritte Zusatzaufgabe implementiert

--- AB2020/src/de/die_gfi/robin/Passwort.java
+++ AB2020/src/de/die_gfi/robin/Passwort.java
@@ -3,33 +3,41 @@ package de.die_gfi.robin;
 import java.util.Arrays;
 import java.util.Scanner;
 import de.die_gfi.util.*;
+import java.util.ArrayList;
 
 public class Passwort {
 	public static void main(String[] args) {
-		
-		System.out.println("Legen Sie ein Passwort fest: ");
+		ArrayList<Integer> passwoerter = new ArrayList<Integer>();
 		Scanner input = new Scanner(System.in);
 		
-		int hash = readPassword(input);
+		
+		while (true) {
+		
+			
+			int hash = readPassword(input);
+		
+			checkIfExists(passwoerter, hash);
 		
 		
-		System.out.println("Geben Sie das Passwort ein: ");
 		
-		int hashNeu = readPassword(input);
+		}
 		
 		
-		if (hash == hashNeu) {
-			System.out.println("Korrekte Eingabe");
+	}
+
+
+	private static void checkIfExists(ArrayList<Integer> passwoerter, int hash) {
+		if (passwoerter.contains(hash) == false) {
+			passwoerter.add(hash);
+			System.out.println("Passwort hinzugefügt");
 		}
 		else
-			System.out.println("Inkorrekte Eingabe");
-		
-		
-		input.close();
+			System.out.println("Dieses Passwort ist bereits vorhanden");
 	}
 
 	
 	private static int readPassword(Scanner input) {
+		System.out.println("Legen Sie ein Passwort fest: ");
 		String passwort = input.next();
 		char[] arr = passwort.toCharArray();
 		int hash = Arrays.hashCode(arr);
