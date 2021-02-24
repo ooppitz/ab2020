64b1aa8d198c715abc7d9c15ba8afdd2b9b19e65
bfz <bfz@HomeO-NB-083101>
2021-02-24 15:02:49 +0100

    verbessertes Layout

--- AB2020/src/de/die_gfi/jonathan/Password.java
+++ AB2020/src/de/die_gfi/jonathan/Password.java
@@ -12,35 +12,34 @@ public class Password {
 		int[] verwendet = new int[10];
 		int zeiger = 0;
 		Arrays.fill(verwendet, 0);
-		boolean work= true;
+		boolean work = true;
 		do {
-		System.out.println
-		("Bitte geben sie login oder paswd an je nachdem welsche aktion sie ausführen möchten");
-		String aktion=sc.next();
-		if (aktion.equalsIgnoreCase("paswd")) {
-			System.out.println("Bitte den Account angeben");
-			zeiger = sc.nextInt();
-			System.out.println("Bitte Passwod eingeben:");
-			pswd[zeiger] = readPassword();
-		} else if (aktion.equalsIgnoreCase("login")) {
-			System.out.println("Bitte den Account angeben");
-			zeiger = sc.nextInt();
-			System.out.println("Bitte Anmelden");
-			int hashEingabe = readPassword();
-			if (hashEingabe == pswd[zeiger]) {
-				System.out.println("Anmeldung erfolgreich\nbis jetzt " + verwendet[zeiger] + " Anmeldungen");
-				verwendet[zeiger]++;
-		}}
-		else {
-			System.out.println("Falsche eingeabe");
-		}
-		
-		System.out.println("Soll da Programm beendet werden");
-		if (sc.next()=="Ja") {
-			work=false;
-		}
-		
-		} while (work==true);
+			System.out.println("Bitte geben sie login oder paswd an je nachdem welsche aktion sie ausführen möchten");
+			String aktion = sc.next();
+			if (aktion.equalsIgnoreCase("paswd")) {
+				System.out.println("Bitte den Account angeben");
+				zeiger = sc.nextInt();
+				System.out.println("Bitte Passwod eingeben:");
+				pswd[zeiger] = readPassword();
+			} else if (aktion.equalsIgnoreCase("login")) {
+				System.out.println("Bitte den Account angeben");
+				zeiger = sc.nextInt();
+				System.out.println("Bitte Anmelden");
+				int hashEingabe = readPassword();
+				if (hashEingabe == pswd[zeiger]) {
+					System.out.println("Anmeldung erfolgreich\nbis jetzt " + verwendet[zeiger] + " Anmeldungen");
+					verwendet[zeiger]++;
+				}
+			} else {
+				System.out.println("Falsche eingeabe");
+			}
+
+			System.out.println("Soll da Programm beendet werden");
+			if (sc.next() == "Ja") {
+				work = false;
+			}
+
+		} while (work == true);
 
 	}
 
