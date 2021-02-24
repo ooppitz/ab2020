a8f955b034806dd6c6e8866b18e5bded25adf77e
bfz <bfz@HomeO-NB-083101>
2021-02-24 14:24:17 +0100

    working

--- AB2020/src/de/die_gfi/jonathan/Password.java
+++ AB2020/src/de/die_gfi/jonathan/Password.java
@@ -8,21 +8,21 @@ public class Password {
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
-		int [] pswd= new int[10];
-		int [] verwendet = new int[10];
-		int zeiger =0;
+		int[] pswd = new int[10];
+		int[] verwendet = new int[10];
+		int zeiger = 0;
 		Arrays.fill(verwendet, 0);
-		
+
 		System.out.println("Bitte den Account angeben");
-		zeiger =sc.nextInt();
+		zeiger = sc.nextInt();
 		System.out.println("Bitte Passwod eingeben:");
 		pswd[zeiger] = readPassword();
 		System.out.println("Bitte den Account angeben");
-		zeiger =sc.nextInt();
+		zeiger = sc.nextInt();
 		System.out.println("Bitte Anmelden");
 		int hashEingabe = readPassword();
 		if (hashEingabe == pswd[zeiger]) {
-			System.out.println("Anmeldung erfolgreich bis jetzt "+verwendet[zeiger]+" Anmeldungen");
+			System.out.println("Anmeldung erfolgreich\nbis jetzt " + verwendet[zeiger] + " Anmeldungen");
 			verwendet[zeiger]++;
 		} else {
 			System.out.println("Anmeldung Fehlgeschlagen");
