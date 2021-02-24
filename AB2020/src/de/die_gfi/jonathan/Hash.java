8b9469babfb56f47571f19b738381e8b6e1c64d4
bfz <bfz@HomeO-NB-083101>
2021-02-24 11:37:51 +0100

    neuformatirt

--- AB2020/src/de/die_gfi/jonathan/Hash.java
+++ AB2020/src/de/die_gfi/jonathan/Hash.java
@@ -3,14 +3,14 @@ package de.die_gfi.jonathan;
 public class Hash {
 
 	public static void main(String[] args) {
-	int b[] =new int [100];
-	for (int i = 0; i < b.length; i++) {
-		b[i]=(int) (Math.random()*1000)-1000;
-	}
-	//int hash=b.hashCode();
-System.out.println(b.hashCode());
-b[30]=90;
-System.out.println(b.hashCode());
+		int b[] = new int[100];
+		for (int i = 0; i < b.length; i++) {
+			b[i] = (int) (Math.random() * 1000) - 1000;
+		}
+		// int hash=b.hashCode();
+		System.out.println(b.hashCode());
+		b[30] = 90;
+		System.out.println(b.hashCode());
 	}
 
 }
