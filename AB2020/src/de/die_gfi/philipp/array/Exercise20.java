bdbec5668dad20b6250aef280bd629bf643155ce
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-02-25 13:06:03 +0100

    Exercise 20 finished, not in an ideal state.

---
+++ AB2020/src/de/die_gfi/philipp/array/Exercise20.java
@@ -0,0 +1,27 @@
+package de.die_gfi.philipp.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 20. Write a Java program to convert an array to ArrayList.
+ */
+public class Exercise20 {
+
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5, 6};
+
+        ArrayList<Integer> arrList = convertArrayToArrayList(arr);
+
+        System.out.print(arrList.toString());
+    }
+
+    public static <E> ArrayList<E> convertArrayToArrayList(E[] array) {
+
+        return new ArrayList<>(Arrays.asList(array));
+    }
+
+
+}
