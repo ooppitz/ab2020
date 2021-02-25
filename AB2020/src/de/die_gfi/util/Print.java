57b3345809a1238fe2baab8bd8d0d568359a97e8
Oliver Oppitz <ooppitz@gmail.com>
2021-02-25 08:50:58 +0100

    Add util.Print.printArray(String[] a)

--- AB2020/src/de/die_gfi/util/Print.java
+++ AB2020/src/de/die_gfi/util/Print.java
@@ -15,4 +15,18 @@ public class Print {
 		System.out.println();
 	}
 	
+	/**
+	 * Prints the content of an array.
+	 * 
+	 * @param array an {@link java.lang.reflect.Array} with non null content
+	 */
+	public static void printArray(String[] array) {
+		
+		for (String value : array) {
+			System.out.print(value + " ");
+		}
+		System.out.println();
+	}
+	
+	
 }
