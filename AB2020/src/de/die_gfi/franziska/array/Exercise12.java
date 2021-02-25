a4b058d3e66a6b4da8242f5b50e8f6dd2a9910d7
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-02-25 16:31:45 +0100

    mistake in logic found. added comment for possible solution later

--- AB2020/src/de/die_gfi/franziska/array/Exercise12.java
+++ AB2020/src/de/die_gfi/franziska/array/Exercise12.java
@@ -18,6 +18,9 @@ public class Exercise12 {
 
 		for (int i = 0; i < arr.length - 1; i++) {
 
+// Fehler bemerkt: funktioniert wahrscheinlich nur, wenn die Elemente direkt nebeneinander im Array liegen
+// Evtl Array sortieren um die Elemente nebeneinander zu schieben, falls sie voneinander entfernt sind und dann nach dem selben Prinzip durchsuchen. 			
+			
 			if (arr[i] == arr[i + 1]) {
 
 				duplicate = true;
