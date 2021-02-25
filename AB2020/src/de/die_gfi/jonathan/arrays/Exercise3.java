bbb1298e05e676b0d8afb3ca71b83040a9f9fd34
bfz <bfz@HomeO-NB-083101>
2021-02-25 10:43:47 +0100

    cosmetic

--- AB2020/src/de/die_gfi/jonathan/arrays/Exercise3.java
+++ AB2020/src/de/die_gfi/jonathan/arrays/Exercise3.java
@@ -6,9 +6,9 @@ import java.util.Arrays;
 import de.die_gfi.util.Print;
 
 public class Exercise3 {
-public static void main(String[] args) {
-	String [] minus=new String[11];
-	Arrays.fill(minus, "\n- - - - - - - - - -");
-	Print.printArray(minus);
-}
+	public static void main(String[] args) {
+		String[] minus = new String[11];
+		Arrays.fill(minus, "\n- - - - - - - - - -");
+		Print.printArray(minus);
+	}
 }
