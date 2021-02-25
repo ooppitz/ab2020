0edb82299b49aee26da75118af32459663a07bf4
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-02-25 13:29:32 +0100

    Exercise 21 somehow finished.

---
+++ AB2020/src/de/die_gfi/philipp/array/Exercise21.java
@@ -0,0 +1,34 @@
+package de.die_gfi.philipp.array;
+
+import de.die_gfi.util.Print;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Exercise21 {
+    public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<>();
+        ArrayList<String> strList = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            intList.add(ThreadLocalRandom.current().nextInt(1, 100));
+            strList.add("Shubbadu");
+        }
+
+        Integer[] intArr = convertArrayListToArray(Integer.class, intList);
+        String[] strArr = convertArrayListToArray(String.class, strList);
+
+        Print.printArray(intArr);
+        Print.printArray(strArr);
+    }
+
+    public static <T> T[] convertArrayListToArray(Class<T> clazz, ArrayList<T> list) {
+        int capacity = list.size();
+
+        @SuppressWarnings("unchecked")
+        T[] array = (T[]) Array.newInstance(clazz, capacity);
+        list.toArray(array);
+        return array;
+    }
+}
