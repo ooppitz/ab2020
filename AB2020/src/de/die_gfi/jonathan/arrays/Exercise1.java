2070419f69209b35c785a659c81a4b77511b673c
bfz <bfz@HomeO-NB-083101>
2021-02-25 09:57:10 +0100

    working

---
+++ AB2020/src/de/die_gfi/jonathan/arrays/Exercise1.java
@@ -0,0 +1,22 @@
+package de.die_gfi.jonathan.arrays;
+
+import java.util.Arrays;
+
+import de.die_gfi.util.Print;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		int [] intArray = {70,3000,5,40,20,35,89,-1,-60,-500,70,8};
+		String [] stringArray = {"Montag","Array","Dienstag","Java","Variante","tippen","lesen","Monat","Programm"};
+		Print.printArray(intArray);
+	Arrays.sort(intArray);
+	Print.printArray(intArray);
+	System.out.println();
+	Print.printArray(stringArray);
+	Arrays.sort(stringArray);
+	Print.printArray(stringArray);
+
+	}
+
+}
