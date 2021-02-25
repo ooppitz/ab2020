1929fa5207cafc4501dd83edf03dff2beab1d487
bfz <bfz@HomeO-NB-083101>
2021-02-25 09:58:00 +0100

    cosmetic

--- AB2020/src/de/die_gfi/jonathan/arrays/Exercise1.java
+++ AB2020/src/de/die_gfi/jonathan/arrays/Exercise1.java
@@ -1,21 +1,22 @@
 package de.die_gfi.jonathan.arrays;
 
 import java.util.Arrays;
-
+//Write a Java program to sort a numeric array and a string array
 import de.die_gfi.util.Print;
 
 public class Exercise1 {
 
 	public static void main(String[] args) {
-		int [] intArray = {70,3000,5,40,20,35,89,-1,-60,-500,70,8};
-		String [] stringArray = {"Montag","Array","Dienstag","Java","Variante","tippen","lesen","Monat","Programm"};
+		int[] intArray = { 70, 3000, 5, 40, 20, 35, 89, -1, -60, -500, 70, 8 };
+		String[] stringArray = { "Montag", "Array", "Dienstag", "Java", "Variante", "tippen", "lesen", "Monat",
+				"Programm" };
 		Print.printArray(intArray);
-	Arrays.sort(intArray);
-	Print.printArray(intArray);
-	System.out.println();
-	Print.printArray(stringArray);
-	Arrays.sort(stringArray);
-	Print.printArray(stringArray);
+		Arrays.sort(intArray);
+		Print.printArray(intArray);
+		System.out.println();
+		Print.printArray(stringArray);
+		Arrays.sort(stringArray);
+		Print.printArray(stringArray);
 
 	}
 
