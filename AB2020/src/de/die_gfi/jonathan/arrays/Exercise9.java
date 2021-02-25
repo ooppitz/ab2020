9bae0c775ff756848514b90787966f06a3ec55af
bfz <bfz@HomeO-NB-083101>
2021-02-25 11:59:54 +0100

    sicherung

---
+++ AB2020/src/de/die_gfi/jonathan/arrays/Exercise9.java
@@ -0,0 +1,10 @@
+//Write a Java program to insert an element (specific position) into an array. 
+package de.die_gfi.jonathan.arrays;
+
+public class Exercise9 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
