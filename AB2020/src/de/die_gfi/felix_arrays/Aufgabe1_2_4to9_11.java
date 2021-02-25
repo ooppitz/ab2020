64ec6a90967b28448bc8ceadf7f40264c6463b75
Felix <Felix@DESKTOP-KRQH503.fritz.box>
2021-02-25 14:01:02 +0100

    Überrsichtsverbesserung

--- AB2020/src/de/die_gfi/felix_arrays/Aufgabe1_2_4to9_11.java
+++ AB2020/src/de/die_gfi/felix_arrays/Aufgabe1_2_4to9_11.java
@@ -6,66 +6,81 @@ import de.die_gfi.util.Print;
 
 public class Aufgabe1_2_4to9_11 { 
 	public static void main(String args[]) {
+		
 		String[] staedte = {"Berlin", "Bern", "Dortmund", "Stutgart", "Wien", "Reichenberg", "Hamburg", "Zürich", "Salzburg", "Luxemburg", "Straßburg", "Frankfurt", "Danzig", "Koeln", "Bozen", "Breslau", "Dresden", "Königsberg", "Oedenburg"};
+		
 		int[] zahlen = {45, 4, 65, 789, 0, -45, 87, 257, 99};
 		
-		//2. sum up values of an array
+		int gesuchterWert = 87;
 		
-		int summe = summeArray(zahlen);
-		System.out.println("Summe: " + summe);
-			
-		//4. calculate the average value of an Array
+		int hinzuzufuegenderWert = 12;
 		
-		int durchschnitt = summe / zahlen.length;
-		System.out.println("durchscnitt: " + durchschnitt);
-	
 		
-		//1. write a java program to sort a numeric array and a string array
 		
 		Arrays.sort(zahlen);
+		
 		Arrays.sort(staedte);
 		
-		System.out.print("Sortierte Städte: ");
-		Print.printArray(staedte);
-		System.out.print("Sortierte Ints: ");
-		Print.printArray(zahlen);
+		int summe = summeArray(zahlen);
 		
-		//5. find out if the array contains a value
-		int gesuchteZahl = 87;
-		existenzPruefen(zahlen, gesuchteZahl);
+		int durchschnitt = durchschnittBerechnen(zahlen, summe);
+			
+		String wertExistiert = ausgabeWertExistiert(zahlen, gesuchterWert);
+				
+		int indexGesuchtesElement = Arrays.binarySearch(zahlen, gesuchterWert);
 		
-		//6. finde den Index zu einem Gesuchten Element
-		
-		int indexGesuchtesElement = Arrays.binarySearch(zahlen, gesuchteZahl);
-		System.out.println("Index: " + indexGesuchtesElement);
-		
-		//7. remove a specific element from an array
-		//8. copy an array by itarating over it
-		//9. add a specific element to an array
-		
-		int wertHinzufuegen = 12;
-		
-		int[] zahlen2 = gesuchtesElementLöschen(zahlen, gesuchteZahl, indexGesuchtesElement);
+		int[] zahlen2 = gesuchtesElementLoeschen(zahlen, gesuchterWert, indexGesuchtesElement);
 		
 		int[] zahlen4 = arrayKopieren(zahlen);
 		
-		int[] zahlen5 = arrayWertHinzufuegen(zahlen, wertHinzufuegen);
-		
-		System.out.println("Sortierte Ints 2: ");
-		Print.printArray(zahlen2);
-		System.out.println("Koppiertes Array");
-		Print.printArray(zahlen4);
-		System.out.println("Sortierte Ints mit hinzugefuegtem Wert: ");
-		Print.printArray(zahlen5);
-		
-		
-		
-		//11. reverse an array of intager values
+		int[] zahlen5 = arrayWertHinzufuegen(zahlen, hinzuzufuegenderWert);
 		
 		int[] zahlen3 = arrayUmdrehen(zahlen);
 		
-		System.out.println("Sortierte Ints umgedreht: ");
+		
+		
+		System.out.print("Sortiertes Intarray: ");
+		Print.printArray(zahlen);
+		
+		System.out.println("Summe aller werte im Intarray: " + summe);
+		
+		System.out.println("durchscnitt aller Werte im Intarray: " + durchschnitt);
+		
+		System.out.println("Kopiertes Array: ");
+		Print.printArray(zahlen4);
+		
+		System.out.println("Sortiertes Int Array mit Gelöschtem Element: ");
+		Print.printArray(zahlen2);
+		
+		System.out.println("Sortierte Ints mit hinzugefuegtem Wert: ");
+		Print.printArray(zahlen5);
+		
+		System.out.println("Sortiertes Intarray umgedreht: ");
 		Print.printArray(zahlen3);
+		
+		System.out.println("Staatus des gesuchten Elements: " + wertExistiert);
+		
+		System.out.println("Index des gesuchten Elements: " + indexGesuchtesElement);
+		
+		System.out.print("Sortiertes String Array: ");
+		Print.printArray(staedte);
+				
+	}
+
+	private static String ausgabeWertExistiert(int[] zahlen, int gesuchteZahl) {
+		boolean wertExistiert = existenzPruefen(zahlen, gesuchteZahl);
+		String rueckgabe = "";
+		if(wertExistiert = true) {
+			rueckgabe = "Wert Existiert";
+		} else {
+			rueckgabe ="Wert Existiert Nicht";
+		}
+		return rueckgabe;
+	}
+
+	private static int durchschnittBerechnen(int[] zahlen, int summe) {
+		int durchschnitt = summe / zahlen.length;
+		return durchschnitt;
 	}
 
 	private static int[] arrayWertHinzufuegen(int[] zahlen, int wertHinzufuegen) {
@@ -86,7 +101,7 @@ public class Aufgabe1_2_4to9_11 {
 		return zahlen4;
 	}
 
-	private static int[] gesuchtesElementLöschen(int[] zahlen, int gesuchteZahl, int indexGesuchtesElement) {
+	private static int[] gesuchtesElementLoeschen(int[] zahlen, int gesuchteZahl, int indexGesuchtesElement) {
 		int laengeZahlen2 = zahlen.length;
 		boolean elementExistiert = existenzPruefen(zahlen,gesuchteZahl);
 		if(elementExistiert) {
@@ -120,11 +135,8 @@ public class Aufgabe1_2_4to9_11 {
 	private static boolean existenzPruefen(int[] zahlen, int gesuchteZahl) {
 		boolean schalter = false;
 		if(Arrays.binarySearch(zahlen, gesuchteZahl) >= 0) {
-			System.out.println("Wert Existiert");
 			schalter = true;
-		} else {
-			System.out.println("Wert Exisitert nicht");
-		}
+			}
 		return schalter;
 	}
 
