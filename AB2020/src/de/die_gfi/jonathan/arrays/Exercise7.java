62df215d5f3dfb64f3a1cc2260b776c87d104cf7
bfz <bfz@HomeO-NB-083101>
2021-02-25 11:35:48 +0100

    Exercise 7 Done

---
+++ AB2020/src/de/die_gfi/jonathan/arrays/Exercise7.java
@@ -0,0 +1,27 @@
+//Write a Java program to remove a specific element from an array.
+package de.die_gfi.jonathan.arrays;
+
+import java.util.Scanner;
+
+import de.die_gfi.util.Print;
+
+public class Exercise7 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] stringArray = { "Montag", "Array", "Dienstag", "Java", "Variante", "tippen", "lesen", "Monat",
+				"Programm" };
+		Print.printArray(stringArray);
+		System.out.println("Bitte den Such Wert angeben");
+		String value = sc.next();
+
+		for (int i = 0; i < stringArray.length; i++) {
+			if (stringArray[i].equals(value)) {
+				stringArray[i] = null;
+			}
+		}
+		Print.printArray(stringArray);
+		sc.close();
+	}
+
+}
