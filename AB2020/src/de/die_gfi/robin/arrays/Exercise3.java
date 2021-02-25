6e897ecb1d7a964ce5e46501480e213d332f168a
Nutzer <Nutzer@DESKTOP-C9INLEB.local>
2021-02-25 09:06:54 +0100

    Aufgabe 3 erster Commit

---
+++ AB2020/src/de/die_gfi/robin/arrays/Exercise3.java
@@ -0,0 +1,15 @@
+package de.die_gfi.robin.arrays;
+
+//3. Write a Java program to print the following grid.
+public class Exercise3 {
+	public static void main(String[] args) {
+		
+		String[] arr = new String[] {"-","-","-","-","-","-","-","-","-","-"};
+		
+		
+		for (int i = 0; i < arr.length; i++) {
+			de.die_gfi.util.Print.printArray(arr);
+		}
+		
+	}
+}
