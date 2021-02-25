d224a815057201524be15b1f5989f5cf8d05b65a
mhhb0570 <mhhb0570@md2krmyc.ad001.siemens.net>
2021-02-25 09:36:53 +0100

    exercise 2 complete

---
+++ AB2020/src/de/die_gfi/eva/arrays/Exercise2.java
@@ -0,0 +1,20 @@
+package de.die_gfi.eva.arrays;
+
+public class Exercise2 {
+	
+	//Write a Java program to sum values of an array.
+
+	public static void main(String[] args) {
+		
+		int[] intArr = {420, 21, 69, 0, 7};
+		int summe = 0;
+		
+		for (int i : intArr) 
+			summe += i;
+		System.out.println("Summe des Arrays = " + summe);	
+		
+		
+
+	}
+
+}
