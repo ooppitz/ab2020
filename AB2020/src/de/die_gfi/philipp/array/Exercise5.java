4315a618ee4deb51c47e4143c3a624b75dbd92d6
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-02-25 08:52:22 +0100

    Exercise 5 finished

---
+++ AB2020/src/de/die_gfi/philipp/array/Exercise5.java
@@ -0,0 +1,24 @@
+package de.die_gfi.philipp.array;
+
+import java.util.Arrays;
+
+/**
+ * 5. Write a Java program to test if an array contains a specific value.
+ */
+public class Exercise5 {
+    public static void main(String[] args) {
+        int[] arr = {53, 95, 78, 154, 49, 276, 44, 98, 76, 88, 69, 32, 110, 34, 35, 87, 94, 121, 589, 382, 190, 583, 238, 382, 19};
+
+        int numberToLookFor = 87;
+
+        int[] cloneArray = arr.clone();
+        Arrays.sort(cloneArray);
+        int indexOfNumber = Arrays.binarySearch(cloneArray, numberToLookFor);
+
+        if (indexOfNumber >= 0) {
+            System.out.println("The number you are looking for is in the array.");
+        } else {
+            System.out.println("The number you are looking for isn't in the array.");
+        }
+    }
+}
