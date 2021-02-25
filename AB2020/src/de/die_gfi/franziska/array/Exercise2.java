4ee9a5c4ff68da9ead4209c923579e463097586a
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-02-25 10:16:56 +0100

    finished exercise 2

--- AB2020/src/de/die_gfi/franziska/array/Exercise2.java
+++ AB2020/src/de/die_gfi/franziska/array/Exercise2.java
@@ -1,7 +1,5 @@
 package de.die_gfi.franziska.array;
 
-import java.util.Arrays;
-
 /**
  * 
  * 2.Write a Java program to sum values of an array.
@@ -14,10 +12,17 @@ public class Exercise2 {
 
 	public static void main(String[] args) {
 
-		int[] arr = {1,6,2};
-		
-		
-		
+		int[] arr = { 1, 6, 2, 1 };
+
+		int sum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			sum = sum + arr[i];
+
+		}
+
+		System.out.println("The sum is: " + sum);
 	}
 
 }
