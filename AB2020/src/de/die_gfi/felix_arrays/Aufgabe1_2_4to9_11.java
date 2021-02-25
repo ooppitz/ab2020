d329c94019efa8b18ee2936b25530438997d7327
Felix <Felix@DESKTOP-KRQH503.fritz.box>
2021-02-25 11:16:13 +0100

    Verbesserungen

--- AB2020/src/de/die_gfi/felix_arrays/Aufgabe1_2_4to9_11.java
+++ AB2020/src/de/die_gfi/felix_arrays/Aufgabe1_2_4to9_11.java
@@ -1,20 +1,17 @@
 package de.die_gfi.felix_arrays;
 
 import java.util.Arrays;
-
+import de.die_gfi.util.Print;
 
 
 public class Aufgabe1_2_4to9_11 { 
 	public static void main(String args[]) {
-		String[] staedte = {"Berlin", "Wien", "Hamburg", "Zürich", "Salzburg", "Luxemburg", "Straßburg", "Frankfurt", "Danzig", "Koeln", "Bozen", "Breslau"};
+		String[] staedte = {"Berlin", "Bern", "Dortmund", "Stutgart", "Wien", "Reichenberg", "Hamburg", "Zürich", "Salzburg", "Luxemburg", "Straßburg", "Frankfurt", "Danzig", "Koeln", "Bozen", "Breslau", "Dresden", "Königsberg", "Oedenburg"};
 		int[] zahlen = {45, 4, 65, 789, 0, -45, 87, 257, 99};
 		
 		//2. sum up values of an array
 		
-		int summe = 0;
-		for(int i = 0; i < zahlen.length; i++) {
-			summe = summe + zahlen[i];
-		}
+		int summe = summeArray(zahlen);
 		System.out.println("Summe: " + summe);
 			
 		//4. calculate the average value of an Array
@@ -28,28 +25,26 @@ public class Aufgabe1_2_4to9_11 {
 		Arrays.sort(zahlen);
 		Arrays.sort(staedte);
 		
-		System.out.println("Sortierte Städte: " + Arrays.toString(staedte));
-		System.out.println("Sortierte Ints: " + Arrays.toString(zahlen));
+		System.out.print("Sortierte Städte: ");
+		Print.printArray(staedte);
+		System.out.print("Sortierte Ints: ");
+		Print.printArray(zahlen);
 		
 		//5. find out if the array contains a value
 		int gesuchteZahl = 87;
-		if(Arrays.binarySearch(zahlen, gesuchteZahl) >= 0) {
-			System.out.println("Wert Existiert");
-		} else {
-			System.out.println("Wert Exisitert nicht");
-		}
+		existenzPrüfen(zahlen, gesuchteZahl);
 		
 		//6. finde den Index zu einem Gesuchten Element
 		
-		int index = Arrays.binarySearch(zahlen, gesuchteZahl);
-		System.out.println("Index: " + index);
+		int indexGesuchtesElement = Arrays.binarySearch(zahlen, gesuchteZahl);
+		System.out.println("Index: " + indexGesuchtesElement);
 		
 		//7. remove a specific element from an array
 		//8. copy an array by itarating over it
 		//9. add a specific element to an array
 		
 		int laengeZahlen2 = zahlen.length;
-		if(index >= 0) {
+		if(indexGesuchtesElement >= 0) {
 			laengeZahlen2 = laengeZahlen2 - 1;
 		} else {
 			laengeZahlen2 = laengeZahlen2 + 1;
@@ -58,22 +53,30 @@ public class Aufgabe1_2_4to9_11 {
 		int k = 0;
 		
 		for(int i = 0; i < zahlen.length; i++) {
-			if(i != index) {
+			if(i != indexGesuchtesElement) {
 				zahlen2[k] = zahlen[i];
 				k++;
 			}
 		}
 		
-		if(index < 0) {
+		if(indexGesuchtesElement < 0) {
 			zahlen2[zahlen2.length - 1] = gesuchteZahl;
 			Arrays.sort(zahlen2);
 		}
 		
-		System.out.println("Sortierte Ints 2: " + Arrays.toString(zahlen2));
+		System.out.println("Sortierte Ints 2: ");
+		Print.printArray(zahlen2);
 		
 		
 		//11. reverse an array of intager values
 		
+		int[] zahlen3 = arrayUmdrehen(zahlen);
+		
+		System.out.println("Sortierte Ints umgedreht: ");
+		Print.printArray(zahlen3);
+	}
+
+	private static int[] arrayUmdrehen(int[] zahlen) {
 		int[] zahlen3 = new int[zahlen.length];
 		int a = 0;
 		int b = zahlen.length - 1;
@@ -83,7 +86,25 @@ public class Aufgabe1_2_4to9_11 {
 			a++;
 			b--;
 		}
-		
-		System.out.println("Sortierte Ints umgedreht: " + Arrays.toString(zahlen3));
+		return zahlen3;
+	}
+
+	private static boolean existenzPrüfen(int[] zahlen, int gesuchteZahl) {
+		boolean schalter = false;
+		if(Arrays.binarySearch(zahlen, gesuchteZahl) >= 0) {
+			System.out.println("Wert Existiert");
+			schalter = true;
+		} else {
+			System.out.println("Wert Exisitert nicht");
+		}
+		return schalter;
+	}
+
+	private static int summeArray(int[] zahlen) {
+		int summe = 0;
+		for(int i = 0; i < zahlen.length; i++) {
+			summe = summe + zahlen[i];
+		}
+		return summe;
 	}
 }
