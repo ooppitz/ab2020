4546d5dc9addc5af0b8245d75548de178befb618
mhhb0570 <mhhb0570@md2krmyc.ad001.siemens.net>
2021-02-25 11:02:18 +0100

    exercise 3 complete

---
+++ AB2020/src/de/die_gfi/eva/arrays/Exercise3.java
@@ -0,0 +1,15 @@
+package de.die_gfi.eva.arrays;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+
+		String[] muster = { "- - - - - - - - - -" };
+
+		for (int i = 0; i < 10; i++) {
+
+			de.die_gfi.util.Print.printArray(muster);
+
+		}
+	}
+}
