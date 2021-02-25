3e787404ae150c5912f4d311800a06e57a5b717b
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-02-25 08:51:52 +0100

    created class exercise 2

---
+++ AB2020/src/de/die_gfi/franziska/array/Exercise2.java
@@ -0,0 +1,23 @@
+package de.die_gfi.franziska.array;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * 2.Write a Java program to sum values of an array.
+ * 
+ * @author Franzi
+ *
+ */
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+
+		int[] arr = {1,6,2};
+		
+		
+		
+	}
+
+}
