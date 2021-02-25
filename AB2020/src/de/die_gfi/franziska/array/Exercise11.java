0035fabfc1ab8b3b1308d6a3bcdc9b73e09f8d18
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-02-25 15:11:05 +0100

    finished exercise 11

--- AB2020/src/de/die_gfi/franziska/array/Exercise11.java
+++ AB2020/src/de/die_gfi/franziska/array/Exercise11.java
@@ -12,10 +12,17 @@ public class Exercise11 {
 
 	public static void main(String[] args) {
 
-		int[] arr = {1,2,3,4,5};
-		
-		int[] arrRev = new int[arr.length];
-		
-	}
+		int[] arr = { 1, 2, 3, 4, 5 };
 
+		int[] arrRev = new int[arr.length];
+
+		for (int i = 0; i < arr.length; i++) {
+
+			arrRev[i] = arr[(arr.length - 1) - i];
+		}
+
+		de.die_gfi.util.Print.printArray(arr);
+
+		de.die_gfi.util.Print.printArray(arrRev);
+	}
 }
