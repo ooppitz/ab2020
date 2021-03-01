9a6e4cad24ca90e659b59aaee0c9e0972f8e4cfb
Oliver Oppitz <ooppitz@gmail.com>
2021-03-01 10:25:15 +0100

    Create class Circle

---
+++ AB2020/src/de/die_gfi/oppitz/Circle.java
@@ -0,0 +1,73 @@
+package de.die_gfi.oppitz;
+
+public class Circle {
+
+	// Radius
+	// Mittelpunkt
+	
+	double radius;                 // Each object has a variable "r"
+	double x;       // Each object has a variable "xCoordinate"
+	double y;
+	
+	public static void main(String[] args) {
+						
+		Circle kreis1 = new Circle(2,3,4);
+		System.out.println("Die Fläche des Kreises #1 ist " + kreis1.calculateArea());
+	 
+		Circle kreis2 = new Circle(4,3,1);
+		System.out.println("Die Fläche des Kreises #2 ist " + kreis2.calculateArea());
+
+		Circle kreis3 = new Circle(14,3,1);
+		System.out.println("Die Fläche des Kreises #3 ist " + kreis3.calculateArea());
+		
+		System.out.println("Der Umfang des Kreises #3 ist " + kreis3.calculateCircumference());
+		
+		System.out.println("Der Radius des Kreises #3 ist " + kreis3.radius);
+
+
+	}
+
+	/** This constructor initialises a Circle object
+	 * 
+	 * @param r
+	 * @param x
+	 * @param y
+	 */
+	public Circle(double r, double x, double y) {
+		
+		this.radius = r; 
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	/** Calculates the area of *this* circle 
+	 * 
+	 * @return the area of this circle
+	 */
+	public double calculateArea() {   // Object method / instance method
+		
+		return this.radius * this.radius * Math.PI;
+	}
+
+	/** Calculate the diameter of this circle 
+	 * 
+	 * @return the diameter
+	 */
+	public double calculateDiameter() {
+		return this.radius * 2;
+	}
+	
+	
+	/** Calculates the circumference of this circle
+	 * 
+	 * @return the circumference
+	 */
+	
+	public double calculateCircumference() {
+		
+		return calculateDiameter() * Math.PI;
+		
+	}
+
+}
