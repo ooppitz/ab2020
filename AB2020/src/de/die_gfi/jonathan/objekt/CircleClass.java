c6032c2ecd51930b27ba292a6c337fddcbbba814
bfz <bfz@HomeO-NB-083101>
2021-03-01 14:29:04 +0100

    sicherung

--- AB2020/src/de/die_gfi/jonathan/objekt/CircleClass.java
+++ AB2020/src/de/die_gfi/jonathan/objekt/CircleClass.java
@@ -3,6 +3,11 @@ import de.die_gfi.oppitz.Circle;
 
 
 public class CircleClass {
+public static void main(String[] args) {
+	Circle c1 = new Circle(4, 5, 6);
+	Circle c2 = new Circle(3, 7, 8);
+	
 
+}
 	
 }
