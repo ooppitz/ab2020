f1b5c9fbc980acd2788267a65c21aa54159bdf6c
bfz <bfz@HomeO-NB-083101>
2021-03-02 16:23:59 +0100

    Commit Quadrat und Rechteck

---
+++ AB2020/src/de/die_gfi/jonathan/Geometry/Quadrat.java
@@ -0,0 +1,19 @@
+package de.die_gfi.jonathan.Geometry;
+
+public class Quadrat extends Rechteck {
+
+	
+	public Quadrat(double seite) {
+		super(seite, seite);
+	}
+	
+	public String toString() {
+		return "Das Quadrat hat eine Seiten Länge von "+laenge+" der Umpfang beträgt "
+	+this.Umfang()+" die Fläche ist "+ this.Flaeche();
+	}
+	public static void main(String[] args) {
+		Quadrat q1 = new Quadrat(2);
+		System.out.println(q1);
+	}
+
+}
---
+++ AB2020/src/de/die_gfi/jonathan/Geometry/Rechteck.java
@@ -0,0 +1,30 @@
+package de.die_gfi.jonathan.Geometry;
+
+public class Rechteck {
+	double laenge;
+	double breite;
+	
+	public Rechteck(double l,double b) {
+		laenge=l;
+		breite=b;
+	}
+	
+	public double Flaeche() {
+		return laenge*breite;
+	}
+	
+	public double Umfang() {
+		return (laenge+breite)*2;
+	}
+	
+	public String toString() {
+		return "Das Rechteck hat eine Länge von "+laenge+" und eine Breite von "+breite+" der"
+				+ " Umpfang beträgt "+this.Umfang()+" die Fläche ist "+ this.Flaeche();
+	}
+	
+	public static void main(String[] args) {
+		Rechteck r1 = new Rechteck(5,2);
+		System.out.println(r1);
+	}
+
+}
