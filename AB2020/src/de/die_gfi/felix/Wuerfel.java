d3cb70d6e2e542feec109a87e21a3d099bece52e
Felix <Felix@DESKTOP-KRQH503.fritz.box>
2021-03-02 14:27:15 +0100

    Implimentieren Unterklasse Wüfel von Quader

---
+++ AB2020/src/de/die_gfi/felix/Wuerfel.java
@@ -0,0 +1,21 @@
+package de.die_gfi.felix;
+
+public class Wuerfel extends Quader {
+	
+	public Wuerfel(double kante) {
+		super(kante, kante, kante );
+	}
+
+	public static void main(String args[]) {
+		Wuerfel wuerfel1 = new Wuerfel(5);
+		System.out.println(wuerfel1.rechneOberflaeche());
+		System.out.println(wuerfel1);
+		
+	}
+	
+	public String toString() {
+		
+		String result = new String("{ Kantenlänge: " + this.tiefe + " }");
+		return result;
+	}
+}
