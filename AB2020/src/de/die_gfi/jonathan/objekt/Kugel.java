24dc071a9252be67dca6eedfe2ab5ef7b449cd8b
bfz <bfz@HomeO-NB-083101>
2021-03-02 13:45:49 +0100

    merge commit

---
+++ AB2020/src/de/die_gfi/jonathan/objekt/Kugel.java
@@ -0,0 +1,22 @@
+package de.die_gfi.jonathan.objekt;
+
+public class Kugel {
+	double radius;
+	double x;
+	double y;
+	double z;
+	
+	public double berechneOberflaeche() {
+		
+	}
+	 
+	 
+	public double berechneDurchmesser(){
+		return radius*2;
+	}
+	public double berechneVolumen() {
+		3/4*m
+	}
+	 public String toString() {}
+	
+}
