a822e9566f9969c663705927b52bf2581cfd6a29
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-02 16:05:51 +0100

    Added small tests of Rectangle and Square

--- AB2020/src/de/die_gfi/philipp/geometry/App.java
+++ AB2020/src/de/die_gfi/philipp/geometry/App.java
@@ -7,5 +7,17 @@ public class App {
 
         System.out.println("The surface area of the cube is " + w.calculateSurfaceArea());
         System.out.println("The volume of the cube " + w + " is " + w.calculateVolume());
+
+        Rectangle rec1 = new Rectangle(5, 3);
+        System.out.println(rec1);
+
+        Square squ1 = new Square(6);
+        System.out.println(squ1);
+
+        System.out.println("Area of rec1 = " + rec1.calculateArea());
+        System.out.println("Area of squ1 = " + squ1.calculateArea());
+
+        System.out.println("Circumference of rec1 = " + rec1.calculateCircumference());
+        System.out.println("Circumference of squ1 = " + squ1.calculateCircumference());
     }
 }
