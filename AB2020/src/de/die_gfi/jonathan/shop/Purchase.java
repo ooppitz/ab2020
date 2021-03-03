d05020b379961374a8698ae8a8eb898a45542e82
bfz <bfz@HomeO-NB-083101>
2021-03-03 09:51:19 +0100

    puarchesitem

--- AB2020/src/de/die_gfi/jonathan/shop/Purchase.java
+++ AB2020/src/de/die_gfi/jonathan/shop/Purchase.java
@@ -1,5 +1,8 @@
 package de.die_gfi.jonathan.shop;
 
+import java.util.ArrayList;
+
 public class Purchase {
+    ArrayList<PurchaseItem> items;
 
 }
---
+++ AB2020/src/de/die_gfi/jonathan/shop/PurchaseItem.java
@@ -0,0 +1,7 @@
+package de.die_gfi.jonathan.shop;
+
+public class PurchaseItem {
+	  Product p; 
+	    int count;
+	   double price;
+}
