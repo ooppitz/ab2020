cc737ccf12821d0dd17313a718cbb8dd40a37955
bfz <bfz@HomeO-NB-083101>
2021-03-03 11:44:06 +0100

    t

--- AB2020/src/de/die_gfi/jonathan/shop/Book.java
+++ AB2020/src/de/die_gfi/jonathan/shop/Book.java
@@ -3,7 +3,7 @@ package de.die_gfi.jonathan.shop;
 public class Book extends Product {
 	int isbn = (int) (Math.random() * 8);
 	String autor;
-//String Titel;
+//String titel;
 	String sprache;
 
 	public Book(String a, String t, String s, double p, int pn) {
