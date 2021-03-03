bb90fc516e7cb245742340e757f176607a0fe125
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-03 09:27:08 +0100

    created class Customer and package shop

---
+++ AB2020/src/de/die_gfi/franziska/shop/Customer.java
@@ -0,0 +1,23 @@
+package de.die_gfi.franziska.shop;
+
+public class Customer {
+	
+	String name;
+	String adresse;
+	String mail;
+	
+
+	public static void main(String[] args) {
+
+		
+		
+		
+	}
+
+	public Customer(String n, String a, String m) {
+		
+		this.name = n;
+		this.adresse = a;
+		this.mail = m;
+	}
+}
