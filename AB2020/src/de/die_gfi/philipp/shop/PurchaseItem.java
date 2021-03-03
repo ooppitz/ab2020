95262b97afc678755ba085126f65b0d20605a56b
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-03 13:00:56 +0100

    Updated PurchaseItem to add two administrative methods.

--- AB2020/src/de/die_gfi/philipp/shop/PurchaseItem.java
+++ AB2020/src/de/die_gfi/philipp/shop/PurchaseItem.java
@@ -1,11 +1,43 @@
 package de.die_gfi.philipp.shop;
 
 public class PurchaseItem {
-    Product prod;
-    int amount;
+    protected Product prod;
+    protected int amount;
 
+    /**
+     * Constructs a {@link PurchaseItem} with a given amount
+     *
+     * @param prod A {@link Product}
+     * @param amount amount of units in the {@link Purchase}
+     */
     public PurchaseItem(Product prod, int amount) {
         this.prod = prod;
         this.amount = amount;
+    }
+
+    /**
+     *
+     * @param prod A {@link Product}
+     */
+    public PurchaseItem(Product prod) {
+        this.prod = prod;
+        this.amount = 1;
+    }
+
+    /**
+     *
+     * @return the article number of the {@link Product} of this {@link PurchaseItem}
+     */
+    public long getArticleNumber() {
+        return prod.articleNumber;
+    }
+
+    /**
+     * Adds the specified amount to this {@link PurchaseItem}
+     *
+     * @param amount Amount to be added
+     */
+    public void addAmount(int amount) {
+        this.amount += amount;
     }
 }
