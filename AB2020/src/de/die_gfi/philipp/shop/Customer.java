5ad85d17c4cd357face5e19acc81c80078e4b62d
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-03 09:26:53 +0100

    Added more info to customer

--- AB2020/src/de/die_gfi/philipp/shop/Customer.java
+++ AB2020/src/de/die_gfi/philipp/shop/Customer.java
@@ -1,13 +1,23 @@
 package de.die_gfi.philipp.shop;
 
+import java.util.ArrayList;
+
 public class Customer {
     public String name;
     public String address;
     public String emailAddress;
+    public int customerNumber;
+    public ArrayList<PurchaseItem> purchases = new ArrayList<>();
+    private static int customers = 0;
 
     public Customer(String name, String address, String emailAddress) {
         this.name = name;
         this.address = address;
         this.emailAddress = emailAddress;
+        this.customerNumber = ++customers;
+    }
+
+    public static int getCustomerAmount() {
+        return customers;
     }
 }
