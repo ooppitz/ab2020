18c4fd04d6fd41f0a708d3d83e9b185275588b7e
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-03 09:19:03 +0100

    Created Customer with initial info

---
+++ AB2020/src/de/die_gfi/philipp/shop/Customer.java
@@ -0,0 +1,13 @@
+package de.die_gfi.philipp.shop;
+
+public class Customer {
+    public String name;
+    public String address;
+    public String emailAddress;
+
+    public Customer(String name, String address, String emailAddress) {
+        this.name = name;
+        this.address = address;
+        this.emailAddress = emailAddress;
+    }
+}
