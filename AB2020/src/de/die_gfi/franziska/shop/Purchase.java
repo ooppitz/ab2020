ce6c31dfb73c56a44b898fcc0ae225ba282805c5
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-03 09:27:31 +0100

    created class Purchase

---
+++ AB2020/src/de/die_gfi/franziska/shop/Purchase.java
@@ -0,0 +1,22 @@
+package de.die_gfi.franziska.shop;
+
+public class Purchase {
+	
+	double menge;
+	double preis;
+
+	public static void main(String[] args) {
+
+		
+		
+		
+	}
+
+	public Purchase(double m, double p) {
+		
+		this.menge = m;
+		this.preis = p;
+		
+	}
+	
+}
