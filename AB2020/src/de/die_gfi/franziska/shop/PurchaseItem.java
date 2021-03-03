bb66620c0602816e092282a8765cd547660624a4
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-03 13:54:45 +0100

    created class PurchaseItem

---
+++ AB2020/src/de/die_gfi/franziska/shop/PurchaseItem.java
@@ -0,0 +1,33 @@
+package de.die_gfi.franziska.shop;
+
+public class PurchaseItem {
+	
+	double preis;
+	int menge;
+	Product p;
+
+	public static void main(String[] args) {
+		
+		Book book1 = new Book("ABC lernen mit Tom", 6.9, 12, 188);
+
+		PurchaseItem item = new PurchaseItem(book1, book1.anzahl , book1.preis);
+		
+		System.out.println(item.toString());
+		
+	}
+	
+	public PurchaseItem(Product p, int m, double preis) {
+		
+		this.preis = preis;
+		this.p = p;
+		this.menge = m;
+		
+	}
+	
+	public String toString() {
+		
+		return  menge + " x " + p.name +  " je " + preis + "€";
+		
+	}
+
+}
