111f4a7615e4d2ba2dc64e8c7c142b451b4cd100
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-03 10:57:08 +0100

    created class Nahrung

---
+++ AB2020/src/de/die_gfi/franziska/shop/Nahrung.java
@@ -0,0 +1,13 @@
+package de.die_gfi.franziska.shop;
+
+public class Nahrung extends Product {
+	
+	String ablaufdatum;
+	
+	public Nahrung(String n, double p, int nr, String a) {
+		
+		
+		
+	}
+
+}
