06a29dec7ac3e1f9190c7a2897e3cd93e8f1c9c8
Felix <Felix@DESKTOP-KRQH503.fritz.box>
2021-03-03 13:42:49 +0100

    Ordnung geschaffen

--- AB2020/src/de/die_gfi/felix/shop/App.java
+++ AB2020/src/de/die_gfi/felix/shop/App.java
@@ -1,59 +1,53 @@
 package de.die_gfi.felix.shop;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class App {
 	
 	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Shop felixOnlineShop = new Shop("Felix Fröhlicher Online Shop", "Kaiser Wilhelm Alee 12, 77777 Hinter-Pusemuckel, Deutschland", 777888999);
 		Custumer kunde1 = new Custumer("Harald", "Harald(at)Harald(dot)com", "Haraldstrasse 1, 77777 Haraldstadt, Schweden");
 		
-		System.out.println(kunde1);
-		
-		
 		Product produkt1 = new Product("Haralds Radiowecker", 12, 77777);
+		Book produkt2 = new Book("Harald Töpfer und der Stein der Weisen", 13, 88888, "JK Rollen", "Fantasy Roman");
+		Food produkt3 = new Food("Haralds Vegane Glutenfreie Mettwurst mit Zwibeln", 14, 99999, 500, true);
 		
-		System.out.println(produkt1);
-		
-		produkt1.displayInformation();
-		
-		
-		Book produkt2 = new Book("Harald Töpfer und der Stein der Weisen", 14, 88888, "JK Rollen", "Fantasy Roman");
-		System.out.println(produkt2);
-		produkt2.displayInformation();
-		
-		Food produkt3 = new Food("Haralds Vegane Glutenfreie Mettwurst mit Zwibeln", 13, 99999, 500, true);
-		System.out.println(produkt3);
-		produkt3.displayInformation();
-		
-		PurchaseItem kauf1 = new PurchaseItem(produkt1, 12);
-		PurchaseItem kauf2 = new PurchaseItem(produkt2, 13);
-		PurchaseItem kauf3 = new PurchaseItem(produkt3, 14);
-		
-		System.out.println(kauf1);
-		
-		ArrayList<PurchaseItem> einkaufswagenListe =new ArrayList<PurchaseItem>();
-		einkaufswagenListe.add(kauf1);
-		einkaufswagenListe.add(kauf2);
-		einkaufswagenListe.add(kauf3);
-		
-		Purchase einkaufswagen = new Purchase(einkaufswagenListe);
-		System.out.println(einkaufswagen);
-			
 		ArrayList<Product> kollecktionListe = new ArrayList<Product>();
 		kollecktionListe.add(produkt1);
 		kollecktionListe.add(produkt2);
 		kollecktionListe.add(produkt3);
 		
-		ProductCollection kollecktion = new ProductCollection(kollecktionListe);
-		System.out.println(kollecktion);
+		Purchase einkauf = einkaufDurchfueren(sc, felixOnlineShop, kollecktionListe);
 		
-		Shop onlineShop = new Shop("Felix Fröhlicher Onlineshop", "Guenthergasse 2 77777 Haraldsstadt Haraldsland", 777888999);
-		System.out.println(onlineShop);
+		System.out.println("Folgende Artikl werden bestellt:");
+		System.out.println(einkauf);
+		System.out.println("Rechnung wird Erstellt");
+		System.out.println(einkauf.erstellenRechnung(kunde1, felixOnlineShop));
 		
-		String rechnung = einkaufswagen.erstellenRechnung(kunde1, onlineShop);
-		System.out.println(rechnung);
-		
+		sc.close();
+	}
 
+	private static Purchase einkaufDurchfueren(Scanner sc, Shop felixOnlineShop, ArrayList<Product> kollecktionListe) {
+		ArrayList<PurchaseItem> einkaufswagen = new ArrayList<PurchaseItem>();
+		
+		System.out.println("Herzlich Willkommen bei " + felixOnlineShop.name);
+		for(int i = 0; i < kollecktionListe.size(); i++) {
+			kollecktionListe.get(i).displayInformation();
+			System.out.println("Moechten Sie diesen Artikel Kaufen? Wenn Ja: xxxJa, Wenn Nein: xxxNein");
+			String eingabe = sc.next();
+			if(eingabe.equals("xxxJa")) {
+				System.out.println("Wie viele von diesm Artiekl möchten Sie kaufen?");
+				int anzahl = sc.nextInt();
+				PurchaseItem posten = new PurchaseItem(kollecktionListe.get(i), anzahl);
+				einkaufswagen.add(posten);
+			}
+		}
+		Purchase einkauf = new Purchase(einkaufswagen);
+		return einkauf;
 	}
 
 }
