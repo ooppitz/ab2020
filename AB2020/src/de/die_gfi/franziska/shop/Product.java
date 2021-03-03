ff49564cd39c07344ea938a84ab67d9616c677df
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-03 09:58:52 +0100

    added code to class Product

--- AB2020/src/de/die_gfi/franziska/shop/Product.java
+++ AB2020/src/de/die_gfi/franziska/shop/Product.java
@@ -1,9 +1,22 @@
 package de.die_gfi.franziska.shop;
 
 public class Product {
+	
+	String name;
+	double preis;
+	int nummer;
+	
 
 	public static void main(String[] args) {
 
 	}
 
+	public Product(String n, double p, int nr){
+		
+		this.name = n;
+		this.preis = p;
+		this.nummer = nr;
+		
+	}
+	
 }
