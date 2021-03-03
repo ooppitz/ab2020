dfa52a1bbf55e52df96aaf623f6963577cb25b5d
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-03 15:21:34 +0100

    First iteration of Shop

--- AB2020/src/de/die_gfi/philipp/shop/Shop.java
+++ AB2020/src/de/die_gfi/philipp/shop/Shop.java
@@ -4,8 +4,45 @@ import java.util.ArrayList;
 
 public class Shop {
 
-    String shopName;
-    ProductCollection products;
-    ArrayList<Customer> customers = new ArrayList<>();
+    protected String shopName;
+    protected String ownerName;
+    protected String shopAddress;
+    protected ProductCollection products;
+    protected ArrayList<Customer> customers;
 
+    public Shop(String shopName, String ownerName, String shopAddress) {
+        this.shopName = shopName;
+        this.ownerName = ownerName;
+        this.shopAddress = shopAddress;
+        this.products = ProductCollection.createProductCollection();
+        this.customers = new ArrayList<>();
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getShopAddress() {
+        return shopAddress;
+    }
+
+    public void setShopAddress(String shopAddress) {
+        this.shopAddress = shopAddress;
+    }
+
+    public ProductCollection getProducts() {
+        return products;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
 }
