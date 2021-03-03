83687e8de39dc671574131e74d9f581817ce13c0
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-03 09:22:38 +0100

    Added the initial necessary info

--- AB2020/src/de/die_gfi/philipp/shop/PurchaseItem.java
+++ AB2020/src/de/die_gfi/philipp/shop/PurchaseItem.java
@@ -1,4 +1,11 @@
 package de.die_gfi.philipp.shop;
 
 public class PurchaseItem {
+    Product prod;
+    int amount;
+
+    public PurchaseItem(Product prod, int amount) {
+        this.prod = prod;
+        this.amount = amount;
+    }
 }
