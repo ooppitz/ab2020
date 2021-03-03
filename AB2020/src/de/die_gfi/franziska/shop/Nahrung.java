3591da9ef36a5e467d55f8cb2a68652e94a0f081
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-03 15:43:31 +0100

    formated class Nahrung

--- AB2020/src/de/die_gfi/franziska/shop/Nahrung.java
+++ AB2020/src/de/die_gfi/franziska/shop/Nahrung.java
@@ -1,13 +1,20 @@
 package de.die_gfi.franziska.shop;
 
 public class Nahrung extends Product {
-	
+
 	String ablaufdatum;
-	
+
 	public Nahrung(String n, double p, int nr, String a) {
-		
-		
-		
+
+		super(n, p, nr);
+
+		this.ablaufdatum = a;
+
 	}
 
+	public String toString() {
+
+		return "Name: " + name + " \nPreis: " + preis + "€" + " \nMenge: " + anzahl + " \nAblaufdatum: " + ablaufdatum;
+
+	}
 }
