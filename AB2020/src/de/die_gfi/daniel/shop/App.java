0a72a3c9e43ec23318a67aa9bbad30b51ef2766b
gfi-daniel <mail@daniel-schmidt.com>
2021-03-03 08:55:49 +0000

    Product.java hinzugefuegt

--- AB2020/src/de/die_gfi/daniel/shop/App.java
+++ AB2020/src/de/die_gfi/daniel/shop/App.java
@@ -4,8 +4,16 @@ public class App
 {
    public static void main(String[] args)
    {
-      Customer daniel = new Customer( "Daniel Schmidt", "Zuhause. 7", "daniel.schmidt@anbieter.de" );
-
+      
+      /* Customer Test */
+      Customer daniel = new Customer( "Daniel Schmidt", "Zuhause. 7, 8000 Muenchen", "daniel.schmidt@anbieter.de" );
       System.out.println( daniel );
+      System.out.println();
+      
+      
+      /* Product Test */
+      Product wecker = new Product( "Analogwecker", 10.0, "Made in Germany", 4189325 );
+      System.out.println( wecker );
+      System.out.println();
    }
 }
