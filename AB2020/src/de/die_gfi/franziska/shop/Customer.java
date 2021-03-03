8cef26ac9a31c37af7b7c36f5e46338273eaed02
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-03 15:43:05 +0100

    formated class Customer

--- AB2020/src/de/die_gfi/franziska/shop/Customer.java
+++ AB2020/src/de/die_gfi/franziska/shop/Customer.java
@@ -1,31 +1,29 @@
 package de.die_gfi.franziska.shop;
 
 public class Customer {
-	
+
 	String name;
 	String adresse;
 	String mail;
-	
 
 	public static void main(String[] args) {
 
 		Customer kunde1 = new Customer("Hans Müller", "Landau", "muel@gmail.com");
-		
+
 		System.out.println(kunde1.toString());
-		
-		
+
 	}
 
 	public Customer(String n, String a, String m) {
-		
+
 		this.name = n;
 		this.adresse = a;
 		this.mail = m;
 	}
-	
+
 	public String toString() {
-		
+
 		return "Name: " + name + "\nAdresse: " + adresse + "\nMail: " + mail;
-		
+
 	}
 }
