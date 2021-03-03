e6d14e19c6ddd518964a900770d215d5fdf9f89e
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-03 15:43:17 +0100

    formated class Customer

--- AB2020/src/de/die_gfi/franziska/shop/Getraenke.java
+++ AB2020/src/de/die_gfi/franziska/shop/Getraenke.java
@@ -1,5 +1,28 @@
 package de.die_gfi.franziska.shop;
 
-public class Getraenke {
+public class Getraenke extends Product {
 
+	String ablaufdatum;
+
+	public Getraenke(String n, double p, int nr, String a) {
+
+		super(n, p, nr);
+
+		this.ablaufdatum = a;
+
+	}
+
+	public static void main(String[] args) {
+
+		Getraenke limo = new Getraenke("Zitronenlimo", 1.99, 498, "22.04.2021");
+
+		System.out.println(limo.toString());
+
+	}
+
+	public String toString() {
+
+		return "Name: " + name + " \nPreis: " + preis + "€" + " \nMenge: " + anzahl + " \nAblaufdatum: " + ablaufdatum;
+
+	}
 }
