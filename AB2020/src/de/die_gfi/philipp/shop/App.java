6fea1e72cad3aa9837635351958ea73982c784be
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-04 09:06:50 +0100

    Updated to ready main() method for the changes in the other classes

--- AB2020/src/de/die_gfi/philipp/shop/App.java
+++ AB2020/src/de/die_gfi/philipp/shop/App.java
@@ -2,9 +2,13 @@ package de.die_gfi.philipp.shop;
 
 import de.die_gfi.philipp.shop.data.*;
 
+import java.util.Scanner;
+
 public class App {
     
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         Shop aShop = TestData.initializeTestShop();
 
         System.out.println(aShop);
