9dd76777c2561da48c7d52dbe165349cba1c15a5
Oliver Oppitz <ooppitz@gmail.com>
2021-03-04 09:54:35 +0100

    Cosmetics

--- AB2020/src/de/die_gfi/felix/shop/Purchase.java
+++ AB2020/src/de/die_gfi/felix/shop/Purchase.java
@@ -3,32 +3,37 @@ package de.die_gfi.felix.shop;
 import java.util.ArrayList;
 
 public class Purchase {
-	
+
 	ArrayList<PurchaseItem> itemList;
 	double preisEinkauf;
-	
-	public Purchase(ArrayList<PurchaseItem> itemList){
+
+	public Purchase(ArrayList<PurchaseItem> itemList) {
 		this.itemList = itemList;
 		double preis1 = 0;
-		for(int i = 0; i < itemList.size(); i++) {
+		for (int i = 0; i < itemList.size(); i++) {
 			preis1 = preis1 + itemList.get(i).preisGesamt;
 			this.preisEinkauf = preis1;
 		}
 	}
-	
+
 	public String toString() {
 		String result = new String();
-		for(int i = 0; i < itemList.size(); i++) {
-			result = result + "Name: " + itemList.get(i).p.name + ", Stueckzahl: " + itemList.get(i).stueckzahl + ", Gesamtpreis Artikel: " + itemList.get(i).preisGesamt + "\n";
+		for (int i = 0; i < itemList.size(); i++) {
+			result = result + "Name: " + itemList.get(i).p.name + ", Stueckzahl: " + itemList.get(i).stueckzahl
+					+ ", Gesamtpreis Artikel: " + itemList.get(i).preisGesamt + "\n";
 		}
-			result = result + "Gesamtpreis: " + this.preisEinkauf;		
+		result = result + "Gesamtpreis: " + this.preisEinkauf;
 		return result;
 	}
-	
+
 	public String erstellenRechnung(Customer c, Shop s) {
+
 		String result = new String();
-		result = result + "RECHNUNG\n" + "Rechnungssteller:\n" + s.toString() + "\n" + "Rechnungsempfänger:\n" + c.toString() + "\n" + this.toString();
+		result = result + "RECHNUNG\n";
+		result += "Rechnungssteller:\n" + s.toString() + "\n";
+		result += "Rechnungsempfänger:\n" + c.toString() + "\n";
+		result += this.toString();
 		return result;
-		
+
 	}
 }
