42850956346c428b3e45a8f4a9d30c67b57a2c97
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-04 08:39:42 +0100

    Added more test data

--- AB2020/src/de/die_gfi/philipp/shop/data/TestData.java
+++ AB2020/src/de/die_gfi/philipp/shop/data/TestData.java
@@ -1,13 +1,18 @@
 package de.die_gfi.philipp.shop.data;
 
+import de.die_gfi.philipp.shop.Customer;
 import de.die_gfi.philipp.shop.Shop;
 
 public class TestData {
 
     public static Shop initializeTestShop() {
-        return new Shop("Hansi's Gemischtwarenladen",
+        Shop shop = new Shop("Hansi's Gemischtwarenladen",
                 "Hansi Mueller",
                 "Dorfstrasse 96, 28392 Dorf im Au");
+        shop.addCustomer("Johannes Mueller", "Muenchner Strasse 74, 80666 Muenchen", "jom58@gmail.com");
+        Customer c = new Customer("Maria Jonasson", "Salzberg 5, 7665 Salzburg", "maria.jo@yahoo.com", shop);
+        shop.addCustomer(c);
+        return shop;
 
     }
 }
