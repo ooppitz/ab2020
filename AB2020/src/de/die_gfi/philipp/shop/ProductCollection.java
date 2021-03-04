e131b315deb32a5ffab42826efb4803640fd205e
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-04 08:31:49 +0100

    Added more products in the default ProductsCollection

--- AB2020/src/de/die_gfi/philipp/shop/ProductCollection.java
+++ AB2020/src/de/die_gfi/philipp/shop/ProductCollection.java
@@ -26,6 +26,9 @@ public class ProductCollection {
         ProductCollection productCollection = new ProductCollection();
         productCollection.addProduct(new Beverage(6839473,"Die Limo", "Granini", 1.39, "2021-05-09", 1000));
         productCollection.addProduct(new Food(6839357, "Salamipralinen", "Rewe Beste Welt",2.99, "2020-03-27", 280));
+        productCollection.addProduct(new Food(6847354, "Butterkeks", "Leibniz", 1.89, "2021-01-05", 400));
+        productCollection.addProduct(new Beverage(6867848, "Apfelsaft", "K Classic", 0.64, "2020-12-13", 1000));
+        productCollection.addProduct(new Product(6802645, "Bleistift", "Staedtler", 3.00, 8));
         return productCollection;
     }
 
