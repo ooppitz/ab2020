0d1bcd7cd300c4eb5a1d64b1d5b8034eb9b37597
Nutzer <Nutzer@DESKTOP-C9INLEB.local>
2021-03-06 21:19:19 +0100

    ye

--- AB2020/src/de/die_gfi/robin/arrays/Exercise16.java
+++ AB2020/src/de/die_gfi/robin/arrays/Exercise16.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 //16. Write a Java program to remove duplicate elements from an array.
 public class Exercise16 {
 	public static void main(String[] args) {
-		int[] arr = new int[] {5,77,23,45,11,22,22,11,5};
+		int[] arr = new int[] {5,77,23,45,11,5,22,22,11,5};
 		Arrays.sort(arr);
-		
+		de.die_gfi.util.Print.printArray(arr);
 		int length = arr.length;
 		int lengthNewArr = length;
 		
-		//Festlegen der Groeße des neuen Arrays
+		//Festlegen der Groeï¿½e des neuen Arrays
 		for (int i = 0; i < length; i++) {
 			if (Arrays.binarySearch(arr, i + 1, length, arr[i]) >= 0) {
 				lengthNewArr--;
@@ -27,8 +27,8 @@ public class Exercise16 {
 				indexNewArr++;
 			}
 		}
-		
 		de.die_gfi.util.Print.printArray(arrNew);
+		
 		
 	}
 }
