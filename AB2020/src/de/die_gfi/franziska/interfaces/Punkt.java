e5fc51e260fabd5c02a7d9d0f49c2e1729ada1ed
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-08 10:16:34 +0100

    added new constructor methods

--- AB2020/src/de/die_gfi/franziska/interfaces/Punkt.java
+++ AB2020/src/de/die_gfi/franziska/interfaces/Punkt.java
@@ -4,30 +4,37 @@ public class Punkt {
 
 	double x;
 	double y;
-	
+
 	public static void main(String[] args) {
 
-		 Punkt p = new Punkt(9);
-		 Punkt q = new Punkt(3, 7);
-		 Punkt3D p1 = new Punkt3D(9,3,2);
-		 Punkt3D  p2 = new Punkt3D(7);
-		 Punkt3D p3 = new Punkt3D(7,4);
+		Punkt p = new Punkt(9);
+		Punkt q = new Punkt(3, 7);
+		Punkt3D p1 = new Punkt3D(9, 3, 2);
+		Punkt3D p2 = new Punkt3D(7);
+		Punkt3D p3 = new Punkt3D(7, 4);
 
 		System.out.println(p.toString());
-		 
+
 	}
 
 	public Punkt(double x, double y) {
-		
+
 		this.x = x;
 		this.y = y;
-		
+
 	}
-	
+
+	public Punkt(double x) {
+
+		this(x, 0); // Hier wird explizit gemacht, dass y = 0 gesetzt wird.
+
+	}
+
+	@Override
 	public String toString() {
-		
+
 		return "x: " + this.x + "y: " + this.y;
-		
+
 	}
-	
+
 }
--- AB2020/src/de/die_gfi/franziska/interfaces/Punkt3D.java
+++ AB2020/src/de/die_gfi/franziska/interfaces/Punkt3D.java
@@ -1,19 +1,34 @@
 package de.die_gfi.franziska.interfaces;
 
-public class Punkt3D extends Punkt{
+public class Punkt3D extends Punkt {
 
 	double z;
-	
+
 	public Punkt3D(double x, double y, double z) {
-		
-		super(x,y);
-		
+
+		super(x, y);
+
 		this.z = z;
-		
+
 	}
-	
+
+	public Punkt3D(double x, double y) {
+
+		this(x, y, 0);
+
+	}
+
+	public Punkt3D(double x) {
+
+		this(x, 0, 0);
+
+	}
+
+	@Override
 	public String toString() {
-		
+
+		return super.toString() + "z: " + this.z;
+
 	}
 
 }
