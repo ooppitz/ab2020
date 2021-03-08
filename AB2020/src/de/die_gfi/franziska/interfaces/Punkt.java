5975a1423d84fdddfd81f24c04c769563cd5850f
Franzi <Franzi@DESKTOP-MGMLUOE.fritz.box>
2021-03-08 09:46:27 +0100

    created package instances, class Punkt and Punkt3D

---
+++ AB2020/src/de/die_gfi/franziska/interfaces/Punkt.java
@@ -0,0 +1,33 @@
+package de.die_gfi.franziska.interfaces;
+
+public class Punkt {
+
+	double x;
+	double y;
+	
+	public static void main(String[] args) {
+
+		 Punkt p = new Punkt(9);
+		 Punkt q = new Punkt(3, 7);
+		 Punkt3D p1 = new Punkt3D(9,3,2);
+		 Punkt3D  p2 = new Punkt3D(7);
+		 Punkt3D p3 = new Punkt3D(7,4);
+
+		System.out.println(p.toString());
+		 
+	}
+
+	public Punkt(double x, double y) {
+		
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	public String toString() {
+		
+		return "x: " + this.x + "y: " + this.y;
+		
+	}
+	
+}
---
+++ AB2020/src/de/die_gfi/franziska/interfaces/Punkt3D.java
@@ -0,0 +1,19 @@
+package de.die_gfi.franziska.interfaces;
+
+public class Punkt3D extends Punkt{
+
+	double z;
+	
+	public Punkt3D(double x, double y, double z) {
+		
+		super(x,y);
+		
+		this.z = z;
+		
+	}
+	
+	public String toString() {
+		
+	}
+
+}
