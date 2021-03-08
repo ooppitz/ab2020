490ee7f430ae1d39ec9e38ca0d1da9795fb6e4ca
Oliver Oppitz <ooppitz@gmail.com>
2021-03-08 14:14:46 +0100

    Changes

--- AB2020/src/de/die_gfi/oppitz/interfaces/Punkt.java
+++ AB2020/src/de/die_gfi/oppitz/interfaces/Punkt.java
@@ -1,36 +1,31 @@
 package de.die_gfi.oppitz.interfaces;
 
-public class Punkt {
+public class Punkt implements CounterInterface {
 
 	double x;
 	double y;
 
 	static int counterPunkt = 0;
+
 	
-	public static void main(String[] args) {
 
-		Punkt p = new Punkt(9);
-		Punkt q = new Punkt(3, 7);
-		Punkt3D p1 = new Punkt3D(9, 3, 2);
-		Punkt3D p2 = new Punkt3D(7);
-		Punkt3D p3 = new Punkt3D(7, 4);
+	public int getCount() {
 
-		Punkt[] array = { p, q, p1, p2, p3}; 
-        for(Punkt pp : array) {
-        	System.out.println(pp);
-        }
-        
-        System.out.println("Es wurden " + Punkt.counterPunkt + " zweidimensionale Punkte erzeugt");
-        System.out.println("Es wurden " + Punkt3D.counterPunkt3D + " dreidimensionale Punkte erzeugt");
-
-        
-        
+		return Punkt.counterPunkt;
 	}
 
+	
+	public int setCount(int newValue) {
+		
+		int oldValue = Punkt.counterPunkt;
+		Punkt.counterPunkt = newValue;
+		return oldValue;
+	}
+	
 	public Punkt(double x, double y) {
 
 		counterPunkt++;
-		
+
 		this.x = x;
 		this.y = y;
 
@@ -38,7 +33,7 @@ public class Punkt {
 
 	public Punkt(double x) {
 
-		this(x, 0);  // Hier wird explizit gemacht, dass y = 0 gesetzt
+		this(x, 0); // Hier wird explizit gemacht, dass y = 0 gesetzt
 
 	}
 
