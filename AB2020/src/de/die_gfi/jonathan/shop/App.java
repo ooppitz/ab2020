777c8576e434e0b694b2f16c0ee379f74f009ca4
Oliver Oppitz <ooppitz@gmail.com>
2021-03-10 14:35:35 +0100

    Extract attribute demo from Shop

--- AB2020/src/de/die_gfi/jonathan/shop/App.java
+++ AB2020/src/de/die_gfi/jonathan/shop/App.java
@@ -3,8 +3,13 @@ package de.die_gfi.jonathan.shop;
 public class App {
 
 	public static void main(String[] args) {
-		Shop s=new Shop();
-		s.d.demo();
-		s.output();
+
+		Shop s = new Shop();
+		
+		Demo demo = new Demo(s.storage, s.users);
+		demo.demo();
+		
+		
+		s.userInterface();
 	}
 }
