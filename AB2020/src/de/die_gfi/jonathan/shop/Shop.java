d27ba7e944a2b231f0769b5057e21a2323d563b1
bfz <bfz@HomeO-NB-083101>
2021-03-10 14:12:34 +0100

    test

--- AB2020/src/de/die_gfi/jonathan/shop/Shop.java
+++ AB2020/src/de/die_gfi/jonathan/shop/Shop.java
@@ -20,62 +20,86 @@ public class Shop {
 		boolean running = true;
 		do {
 
-			Customer c;
 			String account = sc.next();
 			if (account == "Admin") {
+				String input = sc.next().toLowerCase();
+				switch (input) {
+				case "addC":
 
+					break;
+
+				default:
+					break;
+				}
 			} else {
 
-				c = find(account);
-				boolean login = true;
-				do {
-
-					String input = sc.next();
-					if (input.equalsIgnoreCase("buy")) {
-
-						try {
-							buy(sc.nextInt(), c);
-						} catch (InputMismatchException e) {
-							System.out.println("Falsce eingabe kauf vorgang abgebrochen");
-						} catch (IndexOutOfBoundsException e) {
-							System.out.println("Das Produkt existirt nicht");
-						}
-					} else if (input.equalsIgnoreCase("buymult")) {
-						int ram = sc.nextInt();
-						System.out.println("wie viel stück wollen sie kaufen");
-						int timer = sc.nextInt();
-						for (int i = 0; i < timer; i++) {
-							try {
-
-								buy(ram, c);
-							} catch (InputMismatchException e) {
-								System.out.println("Falsce eingabe kauf vorgang abgebrochen");
-							} catch (IndexOutOfBoundsException e) {
-								System.out.println("Das Produkt existirt nicht");
-							}
-						}
-
-					} else if (input.equalsIgnoreCase("print")) {
-						c.printPurchase();
-					} else if (input.equalsIgnoreCase("bill")) {
-						c.createBill();
-					}
-					else if (input.equalsIgnoreCase("index")) {
-						storage.giveIndex();
-					} else if (input.equalsIgnoreCase("logout")) {
-						login = false;
-					} else if (input.equalsIgnoreCase("search")) {
-						storage.search(sc.next());
-					} else if (input.equalsIgnoreCase("exit")) {
-						login = false;
-						running = false;
-						break;
-					}
-				} while (login == true);
+				running = outputCustomer(sc, running, account);
 			}
 
 		} while (running == true);
 		sc.close();
+	}
+
+	public boolean outputCustomer(Scanner sc, boolean running, String account) {
+		Customer c;
+		c = find(account);
+		boolean login = true;
+		System.out.println("Mögliche Eingaben: print search index logout buy buymult exit bill");
+		do {
+
+			String input = sc.next().toLowerCase();
+			switch (input) {
+			case "buy": {
+
+				try {
+					buy(sc.nextInt(), c);
+				} catch (InputMismatchException e) {
+					System.out.println("Falsce eingabe kauf vorgang abgebrochen");
+				} catch (IndexOutOfBoundsException e) {
+					System.out.println("Das Produkt existirt nicht");
+				}
+				break;
+			}
+			case "buymult":
+				int ram = sc.nextInt();
+				System.out.println("wie viel stück wollen sie kaufen");
+				int timer = sc.nextInt();
+				for (int i = 0; i < timer; i++) {
+					try {
+
+						buy(ram, c);
+					} catch (InputMismatchException e) {
+						System.out.println("Falsce eingabe kauf vorgang abgebrochen");
+					} catch (IndexOutOfBoundsException e) {
+						System.out.println("Das Produkt existirt nicht");
+					}
+				}
+				break;
+			case "print":
+				c.printPurchase();
+				break;
+			case "bill":
+				c.createBill();
+				break;
+			case "index":
+				storage.giveIndex();
+				break;
+			case "logout":
+				login = false;
+				break;
+			case "search":
+				storage.search(sc.next());
+				break;
+			case "exit":
+				login = false;
+				running = false;
+				break;
+			default:
+				System.out.println("Mögliche Eingaben: print search index logout buy buymult exit bill");
+			}
+
+		} while (login == true);
+		return running;
 	}
 
 	// suchalgoritmus für die Kunden liste
