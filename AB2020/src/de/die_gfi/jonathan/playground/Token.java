be6a3da5d6c4b0b33b13da7fc11161c144294142
goeth_jonathan <goeth_jonathan@192.168.56.1>
2021-03-11 09:47:56 +0100

    The Playground

---
+++ AB2020/src/de/die_gfi/jonathan/playground/Token.java
@@ -0,0 +1,15 @@
+package de.die_gfi.jonathan.playground;
+
+import java.util.Scanner;
+
+public class Token {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String token1=sc.next();
+	String token2=sc.next();
+	int token3=sc.nextInt();
+	String token4=sc.next();
+	System.out.println(token3);
+	sc.close();
+}
+}
