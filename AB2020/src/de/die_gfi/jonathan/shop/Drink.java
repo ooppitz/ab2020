0cb674f6d7f41e5dac38a21ab878b0518e5edb75
goeth_jonathan <goeth_jonathan@192.168.56.1>
2021-03-12 08:26:38 +0100

    cosmetic

--- AB2020/src/de/die_gfi/jonathan/shop/Drink.java
+++ AB2020/src/de/die_gfi/jonathan/shop/Drink.java
@@ -3,36 +3,37 @@ package de.die_gfi.jonathan.shop;
 public class Drink extends Product implements Discount {
 	boolean Alcohol;
 	String bestBefore;
-	public Drink(String bezeicnbung, double preis ,String minstHalt) {
+
+	public Drink(String bezeicnbung, double preis, String minstHalt) {
 		super(bezeicnbung, preis);
-		//Alcohol=Alkohol;
-		bestBefore=minstHalt;
+		// Alcohol=Alkohol;
+		bestBefore = minstHalt;
 	}
-	
+
 	public String toString() {
 		return "Name: " + name + " Produktnummer: " + produktnumber + " preis: " + price;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	public boolean isDiscountPossible() {
 		return false;
 	}
-	
+
 	public double getMaximumDiscount(int menge) {
-		if (menge>20) {
-			System.out.println(name+" gibt es nur Jetzt 20% billiger");
-			return price-price/20;	
-		}else {
+		if (menge > 20) {
+			System.out.println(name + " gibt es nur Jetzt 20% billiger");
+			return price - price / 20;
+		} else {
 			return price;
 		}
-		
+
 	}
 
 }
