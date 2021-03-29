6ca2af67cc1c6bd506e0483d2b53ddfecaae69b7
Oliver Oppitz <ooppitz@gmail.com>
2021-03-29 10:36:53 +0200

    Changes to playground

--- AB2020/src/de/die_gfi/oppitz/Playground.java
+++ AB2020/src/de/die_gfi/oppitz/Playground.java
@@ -9,20 +9,75 @@ public class Playground {
 
 	public static void main(String[] args) {
 
-		// Wort Buch 3 Berg
-		Scanner sc = new Scanner(System.in);
-		
-		String token = sc.next(); // Token entspricht i.A. einem Wort
-		System.out.println(token);
-		
-		token = sc.nextLine();
-		System.out.println(token);
-		
-		int zahl = sc.nextInt();
-		System.out.println(zahl);
-		
-		
+
+		MyClass instance = new MyClass(1, 7, "Einstein");
+		System.out.println("Name = " + instance.name);
+
+		int staticResult = MyClass.staticAdd(instance);
+
+	
+		System.out.println("Static Result = " + staticResult);
+
 	}
 
+
+}
+
+class MyClass {
+
+	int a;
+	int b;
+	int c;
+
+	String name; 
+	
+	public MyClass() {
+	
+		this.c = -1;
+		this.name = "<the name was not yet set to a specific value>";
+		
+	}
+	
+	public MyClass(int p1, int p2) {
+		
+		this();  // Ruft den Default-Konstruktor auf
+		
+		this.a = p1;
+		this.b = p2;
+	}
+	
+	
+	public MyClass(int a, int b, String name) {
+		
+		this(a,b);
+		
+		this.name = name;
+	}
+	
+	int add() {
+
+		return a + b;
+	}
+
+	int add2() {
+
+		return this.a + this.b;
+	}
+
+	/*
+	 * Jede Objekt-Methode arbeitet auf einem Objekt. Der Pointer this zeigt auf
+	 * dieses Objekt
+	 */
+	int add3() {
+
+		MyClass ref = this;
+		return ref.a + ref.b;
+	}
+
+	/* Eine statische Methode gehört der ganzen Klasse */
+	static int staticAdd(MyClass ref) {
+		
+		return ref.a + ref.b;
+	}
 
 }
