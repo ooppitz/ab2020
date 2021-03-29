1bbf4718eea053afe63df2918adc5827b9edf980
Felix <Felix@DESKTOP-KRQH503.fritz.box>
2021-03-29 13:37:31 +0200

    Konstanten und KOnstruktoren

--- AB2020/src/de/die_gfi/felix/FelixTest.java
+++ AB2020/src/de/die_gfi/felix/FelixTest.java
@@ -1,10 +1,25 @@
 package de.die_gfi.felix;
 
+import java.util.ArrayList;
+
 public class FelixTest {
 	public static void main(String args[]) {
-		System.out.println("Nobody expects the Spanish Inquisition!");
-		System.out.println("For the Empire!");
-		System.out.println("The force is strong in this one!!!");
+		
+		
+		Person p1 = new Person("Gustav");
+		Person p2 = new Person("Harald", "Guentetherson");
+		Person p3 = new Person("Halvar", "von Flake", 1.98);
+		Person p4 = new Person("Manfre Albrecht", "Freiher von Richthofen", 1.80, 70);
+		Person p5 = new Person();
+		ArrayList<Person> pListe = new ArrayList<Person>( ); 
+		pListe.add(p1);
+		pListe.add(p2);
+		pListe.add(p3);
+		pListe.add(p4);
+		pListe.add(p5);
+		for(Person p : pListe) {
+			System.out.println(p.toString());
+		}
 	}
 
 }
---
+++ AB2020/src/de/die_gfi/felix/Person.java
@@ -0,0 +1,62 @@
+package de.die_gfi.felix;
+
+public class Person {
+	
+	private static final double UNKNOWN_DOUBLE = -1;
+	private static final String UNKNOWN_NAME = "";
+	String vorname;
+	String nachname;
+	double koerpergroesse;
+	double koerpergewicht;
+	
+	public Person () {
+		this.vorname = UNKNOWN_NAME;
+		this.nachname = UNKNOWN_NAME;
+		this.koerpergroesse = UNKNOWN_DOUBLE;
+		this.koerpergewicht = UNKNOWN_DOUBLE;
+	}
+	public Person (String vorname) {
+		this.vorname = vorname;
+		this.nachname = UNKNOWN_NAME;
+		this.koerpergroesse = UNKNOWN_DOUBLE;
+		this.koerpergewicht = UNKNOWN_DOUBLE;
+	}
+	public Person (String vorname, String nachname) {
+		this(vorname);
+		this.nachname = nachname;
+		this.koerpergroesse = UNKNOWN_DOUBLE;
+		this.koerpergewicht = UNKNOWN_DOUBLE;
+	}
+	public Person (String vorname, String nachname, double koerpergroesse) {
+		this(vorname, nachname);
+		this.koerpergroesse = koerpergroesse;
+		this.koerpergewicht = UNKNOWN_DOUBLE;
+	}
+	public Person (String vorname, String nachname, double koerpergroesse, double koerpergewicht) {
+		this(vorname, nachname, koerpergroesse);
+		this.koerpergewicht = koerpergewicht;
+	}
+	
+	public String toString() {
+		String s = new String();
+		if(!this.vorname.equals(UNKNOWN_NAME)) {
+			s = s + this.vorname + " ";
+		} else {
+			s = s + "kein Vorname gefunden ";
+		}
+		if(!this.nachname.equals(UNKNOWN_NAME)) {
+			s = s + this.nachname + " ";
+		} else {
+			s = s + "kein Nachname gefunden ";
+		}
+		if(this.koerpergroesse == UNKNOWN_DOUBLE) {
+			s = s + this.koerpergroesse + " ";
+		}
+		if(this.koerpergewicht == UNKNOWN_DOUBLE) {
+			s = s + this.koerpergewicht + " ";
+		}
+		s = s + "/n";
+		return s;
+	}
+	
+}
