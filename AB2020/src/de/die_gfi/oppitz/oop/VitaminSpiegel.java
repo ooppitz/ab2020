138d3963fab8cc6f7b413bcde658d7b4db200ffa
Oliver Oppitz <ooppitz@gmail.com>
2021-03-29 16:15:53 +0200

    Implement second example

---
+++ AB2020/src/de/die_gfi/oppitz/oop/VitaminSpiegel.java
@@ -0,0 +1,14 @@
+package de.die_gfi.oppitz.oop;
+
+public interface VitaminSpiegel {
+	
+	void setVitaminD(double blutwertNgProMl);
+    double getVitaminD();
+
+	void setVitaminA(double blutwertNgProMl);  
+    double getVitaminA();
+
+    void setVitaminC(double blutwertMgProL);
+    double getVitaminC();
+	
+}
