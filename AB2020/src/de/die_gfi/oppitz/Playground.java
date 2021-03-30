355b02c9bbc2798a60015d565199b8401a8daefa
Eva <Eva@DESKTOP-2JPET63.fritz.box>
2021-03-30 09:16:56 +0200

    Remove code to avoid compilation problems

--- AB2020/src/de/die_gfi/oppitz/Playground.java
+++ AB2020/src/de/die_gfi/oppitz/Playground.java
@@ -10,35 +10,7 @@ public class Playground {
 	public static void main(String[] args) {
 		
 		int a = 5; 
-		
-<<<<<<< HEAD
-		System.out.println(a);
-=======
-		Scanner sc = new Scanner(System.in);
-		Scanner sc2 = new Scanner(System.in);
-
-		sc.close();
->>>>>>> refs/remotes/origin/master
-		
-<<<<<<< HEAD
-		System.out.println(a++);  // Post-Inkrement
-		
-		System.out.println(++a);  // Pre-Inkrement
-		
-		System.out.println(--a);  // Pre-Dekrement
-		
-		System.out.println(a--); // Post-Dekrement
-		
-		System.out.println(a);
-		
-<<<<<<< HEAD
-=======
-		String s = sc2.nextLine();
-		System.out.println(s);
->>>>>>> refs/remotes/origin/master
-=======
->>>>>>> branch 'eva.newbranch2.0' of https://github.com/ooppitz/ab2020.git
-		
+	
 	}
 
 }
--- AB2020/src/de/die_gfi/oppitz/geometry/ColoredCircle.java
+++ AB2020/src/de/die_gfi/oppitz/geometry/ColoredCircle.java
@@ -1,59 +1,6 @@
 package de.die_gfi.oppitz.geometry;
 
-public class ColoredCircle extends Circle {
+public class ColoredCircle {
 
-	
-	String color; 
-	String pattern; 
-	
-	
-	public ColoredCircle(double r, double x, double y) {
-		super(r, x, y);
-	}
-
-	public ColoredCircle(double r, double x, double y, String color, String pattern) {
-		super(r, x, y);
-		
-		this.color = color; 
-		this.pattern = pattern;
-		
-	}
-	
-	/** Decide if the specified color fits with this circle 
-	 * 
-	 * @param color
-	 * @return true if the specified color fits, otherwise false
-	 */
-	boolean fitsWith(String matchingColor) {
-		
-		if (matchingColor.equals("black") || matchingColor.equals("white")) {
-			return true;
-		}
-		
-		if (this.color.equals("black") || this.color.equals("white") ) {
-			return true;
-		}
-			
-		return false;
-	}
-	
-	public static void main(String[] args) {
-		
-		ColoredCircle cc = new ColoredCircle(5, 0, 0, "Rot-Weiss", "kariert");
-		
-		System.out.println("Green fits " + cc.fitsWith("green"));
-		System.out.println("White fits " + cc.fitsWith("white"));
-
-		
-		System.out.println("Durchmesser von " + cc + " = " + cc.calculateDiameter());
-		
-	}
-	
-	@Override
-	public String toString() {
-		
-		return super.toString() + " mit der Farbe " + color + " und dem Muster " + pattern;
-	}
-	
 	
 }
