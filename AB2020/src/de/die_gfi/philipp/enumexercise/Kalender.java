1d9fa013c0f80c0dee8f95a17840983d6fec93c5
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-03-31 14:09:38 +0200

    Finished

--- AB2020/src/de/die_gfi/philipp/enumexercise/Kalender.java
+++ AB2020/src/de/die_gfi/philipp/enumexercise/Kalender.java
@@ -36,4 +36,23 @@ public class Kalender {
         }
         return -1;
     }
+
+    public static int berechneTageproMonat(int jahr, int m) {
+        switch (m) {
+            case 1, 3, 5, 7, 8, 10, 12 -> {
+                return 31;
+            }
+            case 2 -> {
+                if (jahr % 100 != 0 && jahr % 4 == 0 || jahr % 400 == 0) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            }
+            case 4, 6, 9, 11 -> {
+                return 30;
+            }
+        }
+        return -1;
+    }
 }
