54ff7fba8409b61e2d04aea7cf22c64c487a3e58
Oliver Oppitz <ooppitz@gmail.com>
2021-03-31 15:07:25 +0200

    Improve Kalendar.java

--- AB2020/src/de/die_gfi/oppitz/oop/enumexercise/Kalender.java
+++ AB2020/src/de/die_gfi/oppitz/oop/enumexercise/Kalender.java
@@ -9,6 +9,29 @@ public class Kalender {
 
 	}
 
+	public static int berechneTageProMonat(int jahr, int m) {
+		
+		Monat monat;
+
+		switch (m) {
+		case 1 -> monat = Monat.JANUAR;
+		case 2 -> monat = Monat.FEBRUAR;
+		case 3 -> monat = Monat.MAERZ;
+		case 4 -> monat = Monat.APRIL;
+		case 5 -> monat = Monat.MAI;
+		case 6 -> monat = Monat.JUNI;
+		case 7 -> monat = Monat.JULI;
+		case 8 -> monat = Monat.AUGUST;
+		case 9 -> monat = Monat.SEPTEMBER;
+		case 10 -> monat = Monat.OKTOBER;
+		case 11 -> monat = Monat.NOVEMBER;
+		case 12 -> monat = Monat.DEZEMBER;
+		}
+		
+		return berechneTageProMonat(jahr, monat);
+
+	}
+
 	public static int berechneTageProMonat(int jahr, Monat m) {
 
 		boolean schaltjahr = (jahr % 4) == 0;
