7acaa95218d7227cfde5e8a5bec8f2474c165350
PilleniusMC <pilleniusmc@mystic-alchemy.com>
2021-04-01 13:47:17 +0200

    Ok, jetzt bin ich genervt

--- AB2020/src/de/die_gfi/philipp/enumexercise/Kalender.java
+++ AB2020/src/de/die_gfi/philipp/enumexercise/Kalender.java
@@ -22,23 +22,28 @@ public class Kalender {
 
         zaehler = berechneTagesNummer(2005, 4, 16);
         System.out.println(zaehler);
-        System.out.println(berechneTagesNummer(2003, Monat.FEBRUAR, 29));
+
         System.out.println(berechneTagesNummer(2003, Monat.MAERZ, 1));
         System.out.println(berechneTagesNummer(2000, 3, 1));
 
         System.out.println("-".repeat(25));
 
         System.out.println(berechneWochentag(1, 1));
-        System.out.println(berechneWochentag(1,2));
+        System.out.println(berechneWochentag(1, 2));
         System.out.println(berechneWochentag(8, 14));
-        System.out.println(berechneWochentag(2, 29));
+
+        System.out.println("-".repeat(25));
+
+        System.out.println(berechneWochentag(1992, 7, 4));
+        System.out.println(berechneWochentag(2023, 8, 12));
+        System.out.println(berechneWochentag(2012, 1, 25));
     }
 
     /**
      * Berechnet die Tage in einem Monat
      *
      * @param jahr Jahresangabe zur Berechnung von Schaltjahren
-     * @param m Ein Monat
+     * @param m    Ein Monat
      * @return 28, 29, 30 oder 31 je nach Monat und ob es ein Schaltjahr ist
      * @throws IllegalArgumentException wenn was schiefgeht
      */
@@ -48,7 +53,7 @@ public class Kalender {
                 return 31;
             }
             case FEBRUAR -> {
-                if (jahr % 100 != 0 && jahr % 4 == 0 || jahr % 400 == 0) {
+                if (checkLeapYear(jahr)) {
                     return 29;
                 } else {
                     return 28;
@@ -65,7 +70,7 @@ public class Kalender {
      * Berechnet die Tage in einem Monat
      *
      * @param jahr Jahresangabe zur Berechnung von Schaltjahren
-     * @param m Ein Monat als numerische Angabe (1 ist Januar, 12 ist Dezember)
+     * @param m    Ein Monat als numerische Angabe (1 ist Januar, 12 ist Dezember)
      * @return 28, 29, 30 oder 31 je nach Monat und ob es ein Schaltjahr ist, -1 wenn etwas schiefgeht
      */
     public static int berechneTageProMonat(int jahr, int m) {
@@ -73,11 +78,24 @@ public class Kalender {
     }
 
     /**
+     *
+     * @param jahr Ein Jahr
+     * @return 366 für ein Schaltjahr, 365 für ein normales Jahr
+     */
+    public static int berechneTageProJahr(int jahr) {
+        if (checkLeapYear(jahr)) {
+            return 366;
+        } else {
+            return 365;
+        }
+    }
+
+    /**
      * Berechnet die Nummer von einem Tag eines bestimmten Monats eines bestimmten Jahres
      *
      * @param jahr Ein Jahr
-     * @param m Ein Monat als Konstante von {@link Monat}
-     * @param tag Ein Tag
+     * @param m    Ein Monat als Konstante von {@link Monat}
+     * @param tag  Ein Tag
      * @return Nummer des Tages zu dem gegebenen vollen Datum, -1 falls ein ung&uuml;ltiges Datum gegeben wurde
      */
     static int berechneTagesNummer(int jahr, Monat m, int tag) {
@@ -88,8 +106,8 @@ public class Kalender {
      * Berechnet die Nummer von einem Tag eines bestimmten Monats eines bestimmten Jahres
      *
      * @param jahr Ein Jahr
-     * @param m Ein Monat als numerische Angabe (1 ist Januar, 12 ist Dezember)
-     * @param tag Ein Tag
+     * @param m    Ein Monat als numerische Angabe (1 ist Januar, 12 ist Dezember)
+     * @param tag  Ein Tag
      * @return Nummer des Tages zu dem gegebenen vollen Datum
      * @throws IllegalArgumentException Wenn ein falsches Datum &uuml;bergeben wurde
      */
@@ -108,7 +126,7 @@ public class Kalender {
     /**
      * Berechnet den Wochentag von einem Tag im Jahr 2021
      *
-     * @param m ein Monat
+     * @param m   ein Monat
      * @param tag ein Tag
      * @return den Wochentag von dem spezifizierten Tag im spezifizierten Monat
      */
@@ -131,7 +149,7 @@ public class Kalender {
     /**
      * Berechnet den Wochentag von einem Tag im Jahr 2021
      *
-     * @param m ein Monat
+     * @param m   ein Monat
      * @param tag ein Tag
      * @return den Wochentag von dem spezifizierten Tag im spezifizierten Monat
      */
@@ -140,24 +158,66 @@ public class Kalender {
     }
 
     /**
+     * Berechnet den Wochentag von einem Tag, beginnened mit "The Epoch" (1970-01-01)
+     *
+     * @param jahr Ein Jahr, 1970 oder gr&ouml;&szlig;er
+     * @param m    Ein Monat
+     * @param tag  Ein Tag
+     * @return Den Wochentag vom gegebenen Datum
+     */
+    static Wochentag berechneWochentag(int jahr, int m, int tag) {
+        int epochYear = 1970;
+        int epochDay = Wochentag.DONNERSTAG.getNumericValue();
+        int eineEinsWeilMagie = 1;
+        if (jahr >= 1970) {
+            int nummerVomGesuchtenTag = berechneTagesNummer(jahr, m, tag);
+            int tageSeitEpoch = 0;
+            for (int j = epochYear; j < jahr; j++) {
+                tageSeitEpoch += berechneTageProJahr(jahr);
+            }
+            tageSeitEpoch += nummerVomGesuchtenTag;
+            int zahlVomWochentag = tageSeitEpoch % 7 + epochDay - eineEinsWeilMagie;
+            if (zahlVomWochentag > 7) {
+                zahlVomWochentag -= 7;
+            }
+            return Wochentag.valueOf(zahlVomWochentag);
+
+        } else {
+            throw new IllegalArgumentException("Year " + jahr + "smaller than 1970, only years later thatn or equal " +
+                    "to 1970 are allowed.");
+        }
+    }
+
+    /**
+     * Berechnet den Wochentag von einem Tag, beginnened mit "The Epoch" (1970-01-01)
+     *
+     * @param jahr Ein Jahr, 1970 oder gr&ouml;&szlig;er
+     * @param m    Ein Monat
+     * @param tag  Ein Tag
+     * @return Den Wochentag vom gegebenen Datum
+     */
+    static Wochentag berechneWochentag(int jahr, Monat m, int tag) {
+        return berechneWochentag(jahr, m.getNumericValue(), tag);
+    }
+
+    /**
      * Pr&uuml;ft ein gegebenes Datum auf G&uuml;ltigkeit
      *
-     * @param jahr Ein Jahr
+     * @param jahr  Ein Jahr
      * @param monat Ein Monat
-     * @param tag Ein Tag
+     * @param tag   Ein Tag
      * @return true wenn das Datum g&uuml;ltig ist, false wenn es ung&uuml;ltig ist
      */
     static boolean checkDate(int jahr, int monat, int tag) {
-        System.out.println(berechneTageProMonat(jahr, monat));
         return (tag >= 1 && tag <= berechneTageProMonat(jahr, monat)) && (monat >= 1 && monat <= 12);
     }
 
     /**
      * Pr&uuml;ft ein gegebenes Datum auf G&uuml;ltigkeit
      *
-     * @param jahr Ein Jahr
+     * @param jahr  Ein Jahr
      * @param monat Ein Monat
-     * @param tag Ein Tag
+     * @param tag   Ein Tag
      * @return true wenn das Datum g&uuml;ltig ist, false wenn es ung&uuml;ltig ist
      */
     static boolean checkDate(int jahr, Monat monat, int tag) {
@@ -168,9 +228,9 @@ public class Kalender {
     /**
      * Diese Methode erzeugt verschiedene Exceptions je nach dem was f&uuml;r ein Fehler gemacht wurde
      *
-     * @param jahr Ein Jahr
+     * @param jahr  Ein Jahr
      * @param monat Ein Monat
-     * @param tag Ein Tag
+     * @param tag   Ein Tag
      * @throws IllegalArgumentException Wenn ein fehlerhaftes Datum &uuml;bergeben wird
      */
     static void errorHandler(int jahr, Monat monat, int tag) {
@@ -180,9 +240,9 @@ public class Kalender {
     /**
      * Diese Methode erzeugt verschiedene Exceptions je nach dem was f&uuml;r ein Fehler gemacht wurde
      *
-     * @param jahr Ein Jahr
+     * @param jahr  Ein Jahr
      * @param monat Ein Monat
-     * @param tag Ein Tag
+     * @param tag   Ein Tag
      * @throws IllegalArgumentException Wenn ein fehlerhaftes Datum &uuml;bergeben wird
      */
     static void errorHandler(int jahr, int monat, int tag) {
@@ -192,7 +252,9 @@ public class Kalender {
 
         int mTage = berechneTageProMonat(jahr, monat);
         if (tag > mTage) {
-            throw new IllegalArgumentException("Tag " + tag + " ist groesser als " + mTage + " fuer den Monat " + monat);
+            if (!checkLeapYear(jahr) && monat == 2 && tag == 29) {
+                throw new IllegalArgumentException("29. Februar ausserhalb von Schaltjahr, " + jahr + " ist kein Schaltjahr");
+            } else throw new IllegalArgumentException("Tag " + tag + " ist groesser als " + mTage + " fuer den Monat " + monat);
         }
 
         if (monat < 1) {
@@ -200,6 +262,20 @@ public class Kalender {
         }
         if (monat > 12) {
             throw new IllegalArgumentException("Monat groesser als 12, maximum erlaubt ist 12");
+        }
+    }
+
+    /**
+     * Pr&uuml;ft ob ein Jahr ein Schaltjahr ist
+     *
+     * @param jahr Ein jahr
+     * @return true wenn das Jahr ein Schaltjahr ist, false wenn es kein Schaltjahr ist
+     */
+    static boolean checkLeapYear(int jahr) {
+        if (jahr % 100 != 0 && jahr % 4 == 0 || jahr % 400 == 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
